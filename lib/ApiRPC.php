ddcbd4a0310665d3523bb1638b2699596954be3f
Romans Malinovskis <r@agiletech.ie>
2006-09-21 07:26:31 +0000

    export RPC server functionality into separate class

--- lib/ApiRPC.php
+++ lib/ApiRPC.php
@@ -1,55 +1,28 @@
 <?
+/*
+ * WARNING:
+ *
+ * This class was created before RPCServer.php. Now it's separated and this class only
+ * works as a wrapper. This separation was done so that you can use your own API and
+ * still be able to handle RPC.
+ *
+ * See RPCServer class
+ */
+
+
 class ApiRPC extends ApiCLI {
-    /*
-     * In order to use this API, all you have to do is to call setHandler
-     * right after initialization. All requests received over the web must
-     * be sent from RPC class.
-     */
 
-    var $handler;
-    var $security_key=null;
-
+    var $rpc_server;
+    function init(){
+        parent::init();
+        $this->rpc_server = $this->add('RPCServer');
+    }
     function setSecurityKey($key){
-        $this->security_key=$key;
+        $this->rpc_server->setSecurityKey($key);
         return $this;
     }
     function setHandler($class_name){
-        try{
-            if(is_object($class_name)){
-                $this->handler=$class_name;
-            }else{
-                $this->handler=$this->add($class_name);
-            }
-            if(!isset($_POST['data']))
-                throw new RPCException('No "data" specified in POST');
-
-            @$data = unserialize(stripslashes($_POST['data']));
-            if($data===false || !is_array($data))
-                throw new RPCException('Data was received, but was corrupted');
-
-            if($this->security_key && count($data)!=3){
-                throw new RPCException('This handler requires security key');
-            }
-
-            if(!$this->security_key && count($data)==3){
-                throw new RPCException('Key was specified but is not required');
-            }
-
-            if(count($data)==3){
-                list($method,$args,$checksum)=$data;
-
-                $rechecksum=md5(serialize(array($method,$args,$this->security_key)));
-                if($rechecksum!=$checksum)
-                    throw new RPCException('Specified security key was not correct');
-            }else{
-                list($method,$args)=$data;
-            }
-
-            $result = call_user_func_array(array($this->handler,$method),$args);
-
-            echo 'AMRPC'.serialize($result);
-        }catch(BaseException $e){
-            echo 'AMRPC'.serialize($e);
-        }
+        $this->rpc_server->setHandler($class_name);
+        return $this;
     }
 }
---
+++ lib/RPCServer.php
@@ -0,0 +1,54 @@
+<?
+class RPCServer extends AbstractController {
+    /*
+     * If you want your API to receive RPC calls, you have to initialize this class
+     * and set handler class and key. Everything else will be performed automatically.
+     */
+
+    var $handler;
+    var $security_key=null;
+
+    function setSecurityKey($key){
+        $this->security_key=$key;
+        return $this;
+    }
+    function setHandler($class_name){
+        try{
+            if(is_object($class_name)){
+                $this->handler=$class_name;
+            }else{
+                $this->handler=$this->add($class_name);
+            }
+            if(!isset($_POST['data']))
+                throw new RPCException('No "data" specified in POST');
+
+            @$data = unserialize(stripslashes($_POST['data']));
+            if($data===false || !is_array($data))
+                throw new RPCException('Data was received, but was corrupted');
+
+            if($this->security_key && count($data)!=3){
+                throw new RPCException('This handler requires security key');
+            }
+
+            if(!$this->security_key && count($data)==3){
+                throw new RPCException('Key was specified but is not required');
+            }
+
+            if(count($data)==3){
+                list($method,$args,$checksum)=$data;
+
+                $rechecksum=md5(serialize(array($method,$args,$this->security_key)));
+                if($rechecksum!=$checksum)
+                    throw new RPCException('Specified security key was not correct');
+            }else{
+                list($method,$args)=$data;
+            }
+
+            $result = call_user_func_array(array($this->handler,$method),$args);
+
+            echo 'AMRPC'.serialize($result);
+        }catch(BaseException $e){
+            echo 'AMRPC'.serialize($e);
+        }
+    }
+}
