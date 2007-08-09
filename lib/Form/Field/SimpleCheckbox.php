99d5fbe9426c113f3a1588c5e75fa635a212045a
Janis Volbergs <j@agiletech.ie>
2007-08-09 02:53:00 +0000

    Generalized checkbox. Similar to it's original role model. Well, if this is removed from trunk, inform me please

---
+++ lib/Form/Field/SimpleCheckbox.php
@@ -0,0 +1,20 @@
+<?php
+class Form_Field_SimpleCheckbox extends Form_Field {
+    function getInput($attr=array()){
+        return parent::getInput(array_merge(
+                    array(
+                        'type'=>'checkbox',
+                        'value'=>'Y',
+                        'checked'=>$this->value=='Y'
+                        ),$attr
+                    ));
+    }
+    function loadPOST(){
+        if(isset($_POST[$this->name])){
+            $this->set('Y');
+        }else{
+            $this->set('');
+        }
+    }
+}
+
