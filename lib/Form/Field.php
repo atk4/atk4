7b77b07d2040745c2e92731c4c3de5a83b369b55
Vladimir Mamzikov <mvs@agiletech.ie>
2007-09-16 17:14:01 +0000

    Money-type field: fix problem with set 0 nullable values.

--- lib/Form/Field.php
+++ lib/Form/Field.php
@@ -337,7 +337,8 @@ class Form_Field_Text extends Form_Field {
 class Form_Field_Money extends Form_Field_Line {
     function load(){
         parent::load();
-        $this->set(number_format(str_replace(',','',$this->value),2));        // formatting
+        if (strlen($this->value)>0)
+        	$this->set(number_format(str_replace(',','',$this->value),2)); // formatting if having value (mvs fix)
     }
 }
 
