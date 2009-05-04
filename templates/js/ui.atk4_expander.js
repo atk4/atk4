1f2b1848b12b820ea10a8f0e62349108c5d0f3ac
Romans Malinovskis <r@agiletech.ie>
2009-05-04 15:02:16 +0000

    add support for inline edit

--- templates/js/ui.atk4_expander.js
+++ templates/js/ui.atk4_expander.js
@@ -9,7 +9,7 @@ $.widget("ui.atk4_expander", {
     _init: function() { 
         var self=this;
         this.element.addClass("ui-atk4-expander");
-        this.element.bind("click.atk4_expander", function(event) { self.click(); });
+        this.element.parent().bind("click.atk4_expander", function(event) { self.click(); });
         //this.element.click(function(){ alert($(this).expanded); $(this).expanded?$(this).collapse():$(this).expand(); });
     },
     click: function() {
@@ -19,6 +19,10 @@ $.widget("ui.atk4_expander", {
     expanded: false,
     expander_id: null,
     expander_url: null,
+    this_tr: null,
+    inline: true,
+    
+    
     id: null,
     expand: function() {
         if(this.expanded)return false;
@@ -28,20 +32,30 @@ $.widget("ui.atk4_expander", {
         this.element.addClass("ui-atk4-expander-active");
 
         // add additional row after ours
-        el=this.element.closest('tr');
+        this.this_tr=this.element.closest('tr');
 
         this.expander_id=this.element.attr('id')+"_ex";
 
-        el.after("<tr id='"+this.expander_id+"'><td colspan="+el.children().length+" class='ui-atk4-expander-bottom'><div>Loading...</div></td></tr>");
+        this.this_tr.after("<tr id='"+this.expander_id+"'><td colspan="+this.this_tr.children().length+" class='ui-atk4-expander-bottom'><div>Loading...</div></td></tr>");
 
         // Kick of annimation before we send request
         var div=$('#'+this.expander_id+' td div');
-        div.animate({height: "200px"},1500);
-        div.load(this.element.attr('rel'),null,function(){
-                div.stop();
-                div.attr('style','display: block'); // clear overflow, height, etc
-                });
-
+        if(this.inline){
+                // inline editor loads contents of <tr>
+            this.this_tr.hide();
+            $('#'+this.expander_id).load(this.element.attr('rel'),null,function(){
+                    div.stop();
+                    div.attr('style','display: block'); // clear overflow, height, etc
+                    $('.expander-close').click(function(){ $('.expander').atk4_expander("collapse"); return false; });
+                    });
+        }else{
+                // expander loands contents of <tr><td><div>
+            div.animate({height: "200px"},1500);
+            div.load(this.element.attr('rel'),null,function(){
+                    div.stop();
+                    div.attr('style','display: block'); // clear overflow, height, etc
+                    });
+        }
         this.expanded=true;
     },
     collapse: function() {
@@ -52,9 +66,16 @@ $.widget("ui.atk4_expander", {
 
         var remove_this=this.expander_id;
 
-        $('#'+this.expander_id+' td div').slideUp("fast",function(){
-                $('#'+remove_this).remove();
-                });
+        if(this.inline){
+            // inline have no div to contract
+            this.this_tr.show();
+            $('#'+remove_this).remove();
+        }else{
+            // expander contracts div
+            $('#'+this.expander_id+' td div').slideUp("fast",function(){
+                    $('#'+remove_this).remove();
+                    });
+        }
 
 
 
