abc1c1bf6766a861f6128861e5153329eb725bad
Romans Malinovskis <r@agiletech.ie>
2009-05-08 17:24:48 +0000

    fixed expander

--- templates/js/ui.atk4_expander.js
+++ templates/js/ui.atk4_expander.js
@@ -5,18 +5,20 @@
  *   ui.core.js
  */
 
-$.widget("ui.atk4_inline", {
+$.widget("ui.atk4_expander", {
     _init: function() {
         var self=this;
-        this.element.parent().bind("click.atk4_inline", function(event) { self.click(); });
+        this.element.addClass("ui-atk4-expander");
+        this.element.parent().bind("click.atk4_expander", function(event) { self.click(); });
+        //this.element.click(function(){ alert($(this).expanded); $(this).expanded?$(this).collapse():$(this).expand(); });
     },
     click: function() {
         if(this.expanded) this.collapse(); else this.expand();
 
     },
     expanded: false,
-    inline_id: null,
-    inline_url: null,
+    expander_id: null,
+    expander_url: null,
     this_tr: null,
     
     
@@ -25,59 +27,39 @@ $.widget("ui.atk4_inline", {
         if(this.expanded)return false;
 
         // Make button look like it's bein pushed
+        this.element.removeClass("ui-atk4-expander");
+        this.element.addClass("ui-atk4-expander-active");
 
         // add additional row after ours
         this.this_tr=this.element.closest('tr');
 
-        this.inline_id=this.element.attr('id')+"_ex";
+        this.expander_id=this.element.attr('id')+"_ex";
 
-        // Substitute current row with a properly formatted "inline editing template"
-        this.this_tr.hide();
-        this.this_tr.after("<tr id='"+this.inline_id+"'><td colspan="+this.this_tr.children().length+"><div class='editrow_rel'><div class='editrow_top'><div></div></div><div class='editrow_left'></div><div class='editrow_right'></div>Loading...</div></td></tr>"
+        this.this_tr.after("<tr id='"+this.expander_id+"'><td colspan="+this.this_tr.children().length+"><div style='height: 0px' id='"+this.expander_id+"'>Loading....</div></td></tr>"
                 );
 
-        // Define local variables (because we will need them in functions)
-        var inline_id_ref=$('#'+this.inline_id);
-
-        // Replace contents of newly added row with AJAX
-        $.ajax({
-            type: "GET",
-            url: this.element.attr('rel'),
-            success: function(res){
-                // Firefox ignores <script>, so we need to extract it and evaluate it
-                // individually. But we need to load HTML first
-                html=res.replace(/<script(.|\s)*?\/script>/g, "");
-                inline_id_ref.html(html);
-
-                // We are going to evaluate each piece, but there is some mess with newlines
-                res
-                .replace(/\n/g,'\uffff')
-                .replace(/<script[^>]*>(.*)<\/script>(.*)/g, function(a,b){ 
-                    x=b.replace(/\uffff/g,'\n'); 
-                    eval(x);
-                    })
-                ;
-
-            }
+        // Kick of annimation before we send request
+        var div=$('#'+this.expander_id+' td div');
+        // expander loands contents of <tr><td><div>
+        div.animate({height: "200px"},1500);
+        div.load(this.element.attr('rel'),null,function(){
+                div.stop();
+                div.attr('style','display: block'); // clear overflow, height, etc
         });
         this.expanded=true;
     },
     collapse: function() {
-        // We are closing ourselves
-
         if(!this.expanded)return false;
 
-        var remove_this=this.inline_id;
+        this.element.removeClass("ui-atk4-expander-active");
+        this.element.addClass("ui-atk4-expander");
 
-        this.this_tr.show();
-            $('#'+remove_this).remove();
-/*            $('#'+remove_this+'2').remove(); */
-        }else{
-            // expander contracts div
-            $('#'+this.expander_id+' td div').slideUp("fast",function(){
-                    $('#'+remove_this).remove();
-                    });
-        }
+        var remove_this=this.expander_id;
+
+        // expander contracts div
+        $('#'+this.expander_id+' td div').slideUp("fast",function(){
+                $('#'+remove_this).remove();
+        });
 
 
 
