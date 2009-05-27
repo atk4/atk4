706a85dfadc96da2375749d1f44d131fe3ae0f85
Romans Malinovskis <r@agiletech.ie>
2009-05-27 10:39:57 +0000

    Text type fields (textarea) will not add double entities. That means if your data is already been through htmlentities, it's not showing up as bunch of &nbsp;s

--- lib/Form/Field.php
+++ lib/Form/Field.php
@@ -442,7 +442,7 @@ class Form_Field_Text extends Form_Field {
 
         return
             parent::getInput(array_merge(array(''=>'textarea'),$attr)).
-            htmlspecialchars(stripslashes($this->value)).
+            htmlspecialchars(stripslashes($this->value),ENT_COMPAT,'ISO-8859-1',false).
             $this->getTag('/textarea');
     }
 }
