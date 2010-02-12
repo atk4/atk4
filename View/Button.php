24085781433c7aea48baa95c68236e4a7ce47b97
Romans Malinovskis <r@agiletech.ie>
2010-02-12 20:12:02 +0000

    moved button from addons

---
+++ View/Button.php
@@ -0,0 +1,66 @@
+<?php
+class View_Button extends View_HtmlElement {
+	private $icon=null;
+	private $link=null;
+    function defaultTemplate(){
+        return array('button','button');
+    }
+	function setIcon($icon){
+		$this->icon=$icon;
+		$this->template->set('icon',$icon);
+	}
+    function setLabel($label){
+        list($label,$icon)=explode(',',$label);
+        $this->setText($label);
+
+        if($icon)$this->setIcon($icon);
+
+        return $this;
+    }
+	function setButtonStyle($n){
+		$this->template->set('button_style',$n);
+		return $this;
+	}
+    function setStyle($key,$value=null){
+		return $this->addStyle($key,$value);
+    	//$this->style[]="$key: $value";
+    	//return $this;
+    }
+	function setLink($link){
+		//$this->addClass('atk4-link');
+		return $this->setAttr('href',$link);
+	}
+    function setClass($class){
+    	$this->class=$class;
+    	return $this;
+    }
+    function render(){
+
+		if($this->icon){
+			$this->addClass('ui-button-and-icon');
+		}else{
+			//$this->addClass('ui-button');
+			$this->template->tryDel('icon_span');
+		}
+
+        return parent::render();
+    }
+	function onClick(){
+		return $this->js('click')->univ();
+	}
+	function setAction($js=null,$page=null){
+		if(strpos($page,'.')===false && strpos($page,':')===false && $page){
+			$page=$this->api->getDestinationUrl($page);
+		}
+		// Set no-js compatibility link
+		if($page)$this->setLink($page);
+
+		// If we have JS, it's a custom action. Otherwise we will load pagelink
+		if($js){
+			$this->js('click',$js);
+		}elseif($page){
+			$this->js('click')->univ()->redirectURL($page);
+		}
+		return $this;
+	}
+}
---
+++ View/HtmlElement.php
@@ -0,0 +1,50 @@
+<?
+/*
+ * This object add one simple element on the page such as div or tag. However it's extremely useful
+ * for adding different properties, events or setting crlass
+ */
+class View_HtmlElement extends View {
+	function setElement($element){
+		$this->template->trySet('element',$element);
+		return $this;
+	}
+	function setAttr($attribute,$value=null){
+		if(is_array($attribute)&&is_null($value)){
+			foreach($attribute as $a=>$b)$this->setAttr($a,$b);
+			return $this;
+		}
+		$this->template->append('attributes',' '.$attribute.'="'.$value.'"');
+		return $this;
+	}
+	function addClass($class){
+		if(is_array($class)){
+			foreach($class as $c)$this->addClass($class);
+			return $this;
+		}
+		$this->template->append('class'," ".$class);
+		return $this;
+	}
+	function addStyle($property,$style=null){
+    	if(is_null($style)&&is_array($property)){
+    		foreach($property as $k=>$v)$this->setStyle($k,$v);
+    		return $this;
+    	}
+		$this->template->append('style',";".$property.':'.$style);
+		return $this;
+	}
+	function setText($text){
+		$this->template->trySet('element_content',$text);
+		return $this;
+	}
+	function set($text){
+		return $this->setText($text);
+	}
+	function pageLink($page){
+		$this->addClass('atk4-link');
+		$this->setAttr('href',$this->api->getDestinationURL($page));
+		return $this;
+	}
+	function defaultTemplate(){
+		return array('htmlelement','_top');
+	}
+}
