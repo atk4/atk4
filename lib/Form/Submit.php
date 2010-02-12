e06fd0df03fe61efca1d9b1298be4ab4ea3f1ccc
Romans Malinovskis <r@agiletech.ie>
2010-02-12 21:19:54 +0000

    make submit use button class

--- lib/Form/Submit.php
+++ lib/Form/Submit.php
@@ -6,20 +6,27 @@
  * @copyright	See file COPYING
  * @version		$Id$
  */
-class Form_Submit extends AbstractView {
-    public $label;
+class Form_Submit extends View_Button {
+    protected $label;	// absolute: TODO: remove
     public $no_save=null;
     protected $style=array();
 
+	/*
     function setLabel($_label){
         $this->label=$_label;
         return $this;
     }
+	*/
+	function init(){
+		parent::init();
+		$this->template->trySet('type','submit');
+	}
     function setNoSave(){
         // Field value will not be saved into defined source (such as database)
         $this->no_save=true;
         return $this;
     }
+	/*
     function render(){
         $this->owner->template_chunks['form']
         	->append('form_buttons','<input type="submit" name="'.$this->name.
@@ -31,4 +38,5 @@ class Form_Submit extends AbstractView {
     	$this->style[] = 'color: '.$color;
     	return $this;
     }
+	*/
 }
