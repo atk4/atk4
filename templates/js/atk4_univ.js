ec1efa9ade1b1d1ff0cfc5ac4e18b799332ba633
Romans Malinovskis <r@agiletech.ie>
2010-04-22 15:08:17 +0000

    add toJSON to global univ

--- templates/js/atk4_univ.js
+++ templates/js/atk4_univ.js
@@ -443,6 +443,88 @@ loadingInProgress: function(){
 },$.univ._import
 );
 
+$.extend($.univ,{
+
+// Function with custom return value
+
+	toJSON: function (value, whitelist) {
+		var m = {
+			'\b': '\\b',
+			'\t': '\\t',
+			'\n': '\\n',
+			'\f': '\\f',
+			'\r': '\\r',
+			'"' : '\\"',
+			'\\': '\\\\'
+		};
+
+        var a,          // The array holding the partial texts.
+            i,          // The loop counter.
+            k,          // The member key.
+            l,          // Length.
+            r = /["\\\x00-\x1f\x7f-\x9f]/g,
+            v;          // The member value.
+        switch (typeof value) {
+        case 'string':
+            return r.test(value) ?
+                '"' + value.replace(r, function (a) {
+                    var c = m[a];
+                    if (c) {
+                        return c;
+                    }
+                    c = a.charCodeAt();
+                    return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
+                }) + '"' :
+                '"' + value + '"';
+        case 'number':
+            return isFinite(value) ? String(value) : 'null';
+        case 'boolean':
+        case 'null':
+            return String(value);
+        case 'object':
+            if (!value) {
+                return 'null';
+            }
+            if (typeof value.toJSON === 'function') {
+                return this.toJSON(value.toJSON());
+            }
+            a = [];
+            if (typeof value.length === 'number' &&
+                    !(value.propertyIsEnumerable('length'))) {
+                l = value.length;
+                for (i = 0; i < l; i += 1) {
+                    a.push(this.toJSON(value[i], whitelist) || 'null');
+                }
+                return '[' + a.join(',') + ']';
+            }
+            if (whitelist) {
+                l = whitelist.length;
+                for (i = 0; i < l; i += 1) {
+                    k = whitelist[i];
+                    if (typeof k === 'string') {
+                        v = this.toJSON(value[k], whitelist);
+                        if (v) {
+                            a.push(this.toJSON(k) + ':' + v);
+                        }
+                    }
+                }
+            } else {
+                for (k in value) {
+                    if (typeof k === 'string') {
+                        v = this.toJSON(value[k], whitelist);
+                        if (v) {
+                            a.push(this.toJSON(k) + ':' + v);
+                        }
+                    }
+                }
+            }
+            return '{' + a.join(',') + '}';
+        }
+    }
+});
+
+
+
 ////// Define deprecated functions ////////////
 $.each([
 	'openExpander'
