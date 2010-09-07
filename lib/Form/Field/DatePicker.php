58afba67005ea82e0a9e9b16c88286b81fcee648
Romans Malinovskis <r@agiletech.ie>
2010-09-07 23:37:05 +0000

    allow to define more options after init

--- lib/Form/Field/DatePicker.php
+++ lib/Form/Field/DatePicker.php
@@ -5,9 +5,12 @@
  * MySQL acceptable date format (YYYY-MM-DD)
  */
 class Form_Field_DatePicker extends Form_Field {
-	function init(){
-		parent::init();
-		$this->js(true)->datepicker(array(
+	public $options=array();
+	function getInput($attr=array()){
+		// $this->value contains date in MySQL format
+		// we need it in locale format
+
+		$this->js(true)->datepicker(array_merge(array(
 					'duration'=>0,
 					//'showOn'=>'button',
 					//'buttonImage'=>'images/calendar.gif',
@@ -15,11 +18,8 @@ class Form_Field_DatePicker extends Form_Field {
 					'changeMonth'=>true,
 					'changeYear'=>true,
 					'dateFormat'=>$this->api->getConfig('locale/date_js','dd/mm/yy')
-					));
-	}
-	function getInput($attr=array()){
-		// $this->value contains date in MySQL format
-		// we need it in locale format
+					),$this->options));
+
 		return parent::getInput(array_merge(
 			array(
 				'value'=>$this->value?(date($this->api->getConfig('locale/date'),strtotime($this->value))):'',
