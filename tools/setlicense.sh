d914d8ab3dd1475f23ed37ba42c8e8443f7910f0
Romans Malinovskis <r@agiletech.ie>
2011-01-30 23:04:09 +0000

    Add script to fix file headers

---
+++ tools/setlicense.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# WARNING:
+#  Agile Toolkit 4 is copyright of Agile Technologies Limited Ireland.
+#  Toolkit it freely available under terms of AGPL license. You are
+#  permitted to download, use it for any project as long as that project
+#  code is licensed under AGPLv3. You must provide source code download
+#  link on your project website.
+#
+#  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#  Using this script to strip off license terms of Agile Toolkit 4
+#  without agreement from copyright holder is a criminal offerce.
+#
+#  Using this code in combination with proprietary code in on-line
+#  (Software as a Service or Application Service) or distribution
+#  of such packages are violation of Licensing terms.
+#  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#
+#  If you are willing to use Agile Toolkit 4 in your project under
+#  other license - please visit http://www.atk4.com/commercial
+#  
+
+[ -f tools/header1 ] || { echo "Run from main atk4 directory"; exit; }
+
+find . -name '*.php' | while read f; do 
+
+echo -n "$f.. "
+
+grep -q '**ATK**' $f || {
+
+vim -e $f <<EOF
+1r tools/header1
+w
+EOF
+echo "added"
+continue
+
+}
+
+
+# Replace license text
+vim -e $f <<EOF
+H
+/\*\*ATK4\*\*\*/,/\*\*\*ATK4\*\*\//-1d
+/\*\*\*ATK4\*\*\//-1r tools/header2
+w
+EOF
+
+echo "updated"
+
+done
