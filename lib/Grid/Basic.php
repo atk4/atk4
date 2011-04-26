d050b624eb22da017c3d6e39d7c072a7352b28b5
Romans Malinovskis <r@agiletech.ie>
2011-04-26 01:56:45 +0100

    improve how grid's link work, now relies on template

--- lib/Grid/Basic.php
+++ lib/Grid/Basic.php
@@ -372,10 +372,17 @@ class Grid_Basic extends CompleteLister {
 		);
 		$this->current_row[$field]=$this->record_order->getCell($this->current_row['id']);
 	}
+    function init_link($field){
+        $this->setTemplate('<a href="<?$_link?>"><?$'.$field.'?></a>');
+    }
 	function format_link($field){
+        $this->current_row['_link']=$this->api->getDestinationURL('./details',array('id'=>$this->current_row['id']));
+        return $this->format_template($field);
+        /*
 		$this->current_row[$field]='<a href="'.$this->api->getDestinationURL($field,
 			array('id'=>$this->current_row['id'])).'">'.
 			$this->columns[$field]['descr'].'</a>';
+            */
 	}
 	function _performDelete($id){
 		$this->dq->where($this->dq->args['table'].'.id',$id)->do_delete();
@@ -853,6 +860,7 @@ class Grid_Basic extends CompleteLister {
 		}
 		$this->columns[$field]['type']=$formatter;
 		if(method_exists($this,$m='init_'.$formatter))$this->$m($field);
+        $this->last_column=$field;
         return $this;
 	}
 }
