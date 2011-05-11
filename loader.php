108c1483476d827e68a2344601ecc4216ec532ea
Romans Malinovskis <r@agiletech.ie>
2011-05-11 10:36:06 +0100

    fix for phpfog

--- loader.php
+++ loader.php
@@ -1,5 +1,6 @@
 <?php
 // This sets initial path for the include. Further includes will be initialized through PathFinder
 set_include_path('.'.PATH_SEPARATOR.'.'.DIRECTORY_SEPARATOR.'lib'.PATH_SEPARATOR.dirname(__FILE__).'/lib');
-include dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'static.php';
+include 'static.php';
+//include dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'static.php';
 ?>
