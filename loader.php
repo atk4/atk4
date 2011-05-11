fafdffb93904093469b89dfd6a0d02d2299f7f3c
Romans Malinovskis <r@agiletech.ie>
2011-05-11 10:31:00 +0100

    use safer set_include_path funciton

--- loader.php
+++ loader.php
@@ -1,5 +1,5 @@
 <?php
 // This sets initial path for the include. Further includes will be initialized through PathFinder
-ini_set('include_path','.'.PATH_SEPARATOR.'.'.DIRECTORY_SEPARATOR.'lib'.PATH_SEPARATOR.dirname(__FILE__).'/lib');
+set_include_path('include_path','.'.PATH_SEPARATOR.'.'.DIRECTORY_SEPARATOR.'lib'.PATH_SEPARATOR.dirname(__FILE__).'/lib');
 include dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'static.php';
 ?>
