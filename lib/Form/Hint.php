4d910bb666ad4dba09078f1eaac6fe9153ad7bab
Romans Malinovskis <r@agiletech.ie>
2011-07-05 15:11:54 +0100

    Update headers / homepage
    changed from atk4.com to agiletoolkit.org

--- lib/Form/Hint.php
+++ lib/Form/Hint.php
@@ -7,7 +7,7 @@
 
  **ATK4*****************************************************
    This file is part of Agile Toolkit 4 
-    http://www.atk4.com/
+    http://agiletoolkit.org
   
    (c) 2008-2011 Agile Technologies Ireland Limited
    Distributed under Affero General Public License v3
@@ -19,7 +19,7 @@
    See LICENSE.txt for more information
 
    You can obtain non-public copy of Agile Toolkit 4 at
-    http://www.atk4.com/commercial/ 
+    http://agiletoolkit.org/commercial
 
  *****************************************************ATK4**/
 class Form_Hint extends View_HtmlElement {
--- lib/H3.php
+++ lib/H3.php
@@ -1,2 +1,25 @@
 <?php
+/***********************************************************
+   ..
+
+   Reference:
+     http://atk4.com/doc/ref
+
+ **ATK4*****************************************************
+   This file is part of Agile Toolkit 4 
+    http://agiletoolkit.org
+  
+   (c) 2008-2011 Agile Technologies Ireland Limited
+   Distributed under Affero General Public License v3
+   
+   If you are using this file in YOUR web software, you
+   must make your make source code for YOUR web software
+   public.
+
+   See LICENSE.txt for more information
+
+   You can obtain non-public copy of Agile Toolkit 4 at
+    http://agiletoolkit.org/commercial
+
+ *****************************************************ATK4**/
 class H3 extends HtmlElement { function init(){ parent::init(); $this->setElement('H3'); } }
