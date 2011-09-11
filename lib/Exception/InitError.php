db44e341b6ca9c5b281f2c45d91efe14ff0baac1
Romans Malinovskis <r@agiletech.ie>
2011-09-11 20:27:42 +0100

    Move locally used exceptions from addons

---
+++ lib/Exception/InitError.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Thrown when object initialization sequence was wrong (e.g. object referenced before it is initialized)
+ * or object is initialized in wrong way (e.g. string value instead of integer)
+ * 
+ * @author Camper (cmd@adevel.com) on 02.04.2009
+ */
+class Exception_InitError extends BaseException{}
