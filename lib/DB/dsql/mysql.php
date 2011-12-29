f1615e3ed1606f1978d84048e9a7a414a2fabba9
Janis Volbergs <j@agiletech.ie>
2011-12-29 05:54:50 +0200

    Fixed error in limit parser

--- lib/DB/dsql/mysql.php
+++ lib/DB/dsql/mysql.php
@@ -10,7 +10,7 @@ class DB_dsql_mysql extends DB_dsql {
             return 'limit '.
                 (int)$this->args['limit']['shift'].
                 ', '.
-                (int)$this->args['limit']['shift'];
+                (int)$this->args['limit']['cnt'];
         }
     }
 }
