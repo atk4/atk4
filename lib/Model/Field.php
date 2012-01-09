4c864152f0aed130bbd3f3ccd107ef004b3d9e2a
Romans Malinovskis <r@agiletech.ie>
2012-01-09 16:35:44 +0000

    Rename Model_Filed into simply Field

--- lib/Model/Field.php
+++ lib/Model/Field.php
@@ -1,6 +1,6 @@
 <?php
 
-class Model_Field extends AbstractModel {
+class Field extends AbstractModel {
     public $system=false;
     public $hidden=false;
     public $editable=true;
@@ -31,6 +31,7 @@ class Model_Field extends AbstractModel {
 
     function type($t=undefined){ return $this->setterGetter('type',$t); }
     function caption($t=undefined){ if($t===undefined && !$this->caption)return ucwords(strtr($this->short_name,'_',' ')); return $this->setterGetter('caption',$t); }
+    function group($t=undefined){ return $this->setterGetter('group',$t); }
     function readonly($t=undefined){ return $this->setterGetter('readonly',$t); }
     function mandatory($t=undefined){ return $this->setterGetter('mandatory',$t); }
     function editable($t=undefined){ return $this->setterGetter('editable',$t); }
@@ -45,16 +46,17 @@ class Model_Field extends AbstractModel {
     function visible($t=undefined){ return $this->setterGetter('visible',$t); }
     function listData($t=undefined){ return $this->setterGetter('listData',$t); }
     // what is alias?
-    function alias($t=undefined){ return $this->setterGetter('alias',$t); }
+    //function alias($t=undefined){ return $this->setterGetter('alias',$t); }
 
     function updateSelectQuery($select){
         $select->field($this->short_name);
         return $this;
     }
     // OBSOLETE
+    // TODO: refactor this!
     function refModel($m){
         $this->destroy();
-        $fld = new Model_Field_Reference();
+        $fld = $this->add('Field_Reference');
 
         foreach((Array)$this as $key=>$val){
             $fld->$key=$val;
@@ -69,7 +71,7 @@ class Model_Field extends AbstractModel {
         if($v===false)return $this;
 
         $this->destroy();
-        $fld = new Model_Field_Expression();
+        $fld = $this->add('Field_Expression');
 
         foreach((Array)$this as $key=>$val){
             $fld->$key=$val;
