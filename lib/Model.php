324f1daf3c5ac4e2b9b79d0674c71d14fb327861
Romans Malinovskis <r@agiletech.ie>
2012-01-09 16:34:14 +0000

    Added lots of in-line documentation

--- lib/Model.php
+++ lib/Model.php
@@ -1,23 +1,66 @@
-<?php
+<?php // vim:ts=4:sw=4:et:fdm=marker
 /**
-   * Implementation of the model where you can define fields, conditions, validations
-   * but does not related with any storage media
-   */
+ * Implementation of a Generic Model. 
+ * @link http://agiletoolkit.org/doc/model
+ *
+ * Model has fields which you add with addField() and access through get() and set()
+ * You can also load and save model through different storage controllers.
+ *
+ * This model is designed to work with linear, non-SQL resources, if you are looking
+ * to have support for joins, ordering, advanced SQL syntax, look into Model_Table
+ *
+ * It's recommended that you create your own model class based on generic model where
+ * you define fields, but you may also use instance of generic model.
+ *
+ * Use:
+ * class Model_PageCache extends Model {
+ *     function init(){
+ *         parent::init();
+ *         $this->addField('content')->allowHtml(true);
+ *     }
+ *     function generateContent(){
+ *         //complex computation
+ *         // ...
+ *         $this->set('content',$content);
+ *     }
+ * }
+ *
+ *
+ * $pc=$this->add('Model_PageCache')->setSource('Memcached');
+ * $pc->load($this->api->page);
+ *
+ * if(!$pc->loaded()){
+ *     $pc->set('page',$this->api->page');
+ *     $pc->generateContent();
+ *     $pc->save();
+ * }
+ *
+ *
+ * @license See http://agiletoolkit.org/about/license
+ * 
+**/
 class Model extends AbstractModel implements ArrayAccess,Iterator {
 
     public $default_exception='Exception';
 
+    /** The class prefix used by addField */
     public $field_class='Model_Field';
+
+    /** If true, model will now allow to set values for non-existant fields */
+    public $strict_fields=false;
+
+    /** Contains information about table/file/bucket/array used by Controller to determine source */
+    public $table;
+
+    /** Contains identifier of currently loaded record or null. Use load() and reset() */
+    public $id=null;     // currently loaded record
 
     // Curretly loaded record
     public $data=array();
     public $dirty=array();
 
-    // Information about where data came from
-    public $table;
-    public $id;     // currently loaded record
 
-
+    // {{{ Basic functionality, field definitions, set(), get() and related methods
     function init(){
         parent::init();
 
@@ -25,10 +68,13 @@ class Model extends AbstractModel implements ArrayAccess,Iterator {
             throw $this->exception('model->defineField() is obsolete. Change to init()','Obsolete')
             ->addMoreInfo('class',get_class($this));
     }
+    /** Creates field definition object containing field meta-information such as caption, type
+     * validation rules, default value etc */
     function addField($name){
         return $this
             ->add($this->field_class,$name);
     }
+    /** Set value of the field. If $this->strict_fields, will throw exception for non-existant fields. Can also accept array */
     function set($name,$value=undefined){
         if(is_array($name)){
             foreach($name as $key=>$val)$this->set($key,$val);
@@ -39,7 +85,8 @@ class Model extends AbstractModel implements ArrayAccess,Iterator {
         }
 
         // Verify if such a filed exists
-        //if(!$this->hasElement($name))throw $this->exception('No such field','Logic')->addMoreInfo('name',$name);
+        if($this->strict_fields && !$this->hasElement($name))throw $this->exception('No such field','Logic')
+            ->addMoreInfo('name',$name);
 
         if($value!==undefined){
             $this->data[$name]=$value;
@@ -47,18 +94,60 @@ class Model extends AbstractModel implements ArrayAccess,Iterator {
         }
         return $this;
     }
+    /** Return value of the field. If unspecified will return array of all fields.  */
+    function get($name=null){
+        if($name===null)return $this->data;
+        if($this->strict_fields && !$this->hasElement($name))
+            throw $this->exception('No such field','Logic')->addMoreInfo('field',$name);
+        if(!isset($this->data[$name]))
+            throw $this->exception('Model field was not loaded')
+            ->addMoreInfo('id',$this->id)
+            ->addMoreinfo('field',$name);
+        return $this->data[$name];
+    }
+    /** When fields are changed, they are marked dirty. Only dirty fields are saved when save() is called */
     function setDirty($name){
         $this->dirty[$name]=true;
     }
-
+    /** Returns if the records has been loaded successfully */
     function loaded(){
         return (boolean)$this->data;
     }
-
-    function reset(){
+    /** Forget loaded data */
+    function unload(){
         $this->data=$this->dirty=array();
         return $this;
     }
+    function reset(){
+        return $this->unload();
+    }
+    // }}}
+
+    /// {{{ Operation with external Data Controllers
+    /** Associates appropriate controller and loads data such as 'Array' for Controller_Data_Array class */
+    function setSource($controller, $table=null, $id=null){
+        if(is_string($controller))$controller='Data_'.$controller;
+        $this->setController($controller);
+
+        $this->controller->setSource($this,$table);
+
+        if($id)$this->load($id);
+        return $this;
+    }
+    /** Attempt to load record with specified ID. If this fails, no error is produced */
+    function load($id){
+        return $this->contoller->load($this,$id);
+    }
+    /** Saves record with current controller. If no argument is specified, uses $this->id. Specifying "false" will create 
+     * record with new ID. Returns ID of saved record */
+    function save($id=null){
+        return $this->contoller->save($this,$id);
+    }
+    /** Deletes record associated with specified $id. If not specified, currently loaded record is deleted (and unloaded) */
+    function delete($id=null){
+        return $this->contoller->delete($this,$id?:$this->id);
+    }
+    /// }}}
 
     // {{{ ArrayAccess support 
     function offsetExists($name){
@@ -74,20 +163,6 @@ class Model extends AbstractModel implements ArrayAccess,Iterator {
         unset($this->dirty[$name]);
     }
     // }}}
-
-    /** Associates appropriate controller and loads data */
-    function setSource($controller, $table=null, $id=null){
-        if(is_string($controller))$controller='Data_'.$controller;
-        $this->setController($controller);
-
-        $this->controller->setSource($this,$table);
-
-        if($id)$this->load($id);
-    }
-
-    function load($id){
-        return $this->contoller->load($this,$id);
-    }
 
     // {{{ Iterator support 
     function rewind(){
@@ -108,22 +183,15 @@ class Model extends AbstractModel implements ArrayAccess,Iterator {
     }
     // }}}
 
-    function get($name=undefined){
-        if($name===undefined)return $this->data;
-        //if(!$this->hasElement($name))throw $this->exception('No such field','Logic')->addMoreInfo('name',$name);
-        return $this->data[$name];
-    }
-
 
     // TODO: worry about cloning!
     function newField($name){
         return $this->addField($name); 
     }
     function getEntityCode(){
-        return $this->entity_code;
+        return $this->table?:$this->entity_code;
     }
     function getField($f){
         return $this->getElement($f);
     }
-
 }
