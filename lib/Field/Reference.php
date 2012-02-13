818b8101217b068fefe86f3bd6870cd4e186b2c2
Romans Malinovskis <r@agiletech.ie>
2012-02-13 17:15:33 +0000

    fix dereferencing of null fields

--- lib/Field/Reference.php
+++ lib/Field/Reference.php
@@ -20,6 +20,11 @@ class Field_Reference extends Field {
             $this->model=preg_replace('|^(.*/)?(.*)$|','\1Model_\2',$this->model);
             $this->model=$this->add($this->model);
         }
+        if(!$this->get())throw $this->exception('Reference field has no value')
+            ->addMoreInfo('model',$this->owner)
+            ->addMoreInfo('field',$this->short_name)
+            ->addMoreInfo('id',$this->owner->id)
+            ;
         return $this->model->load($this->get());
     }
     function getDereferenced(){
