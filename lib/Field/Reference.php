b73345b264e0e6778e8df8e4a37a54c90e6cbb33
Romans Malinovskis <r@agiletech.ie>
2012-04-10 17:19:59 +0100

    Implemented multipe ref() modes

--- lib/Field/Reference.php
+++ lib/Field/Reference.php
@@ -1,11 +1,15 @@
 <?php
 class Field_Reference extends Field {
-    public $model;
+    public $model_name=null;
     public $display_field=null;
     public $dereferenced_field=null;
 
     function setModel($model,$display_field=null){
-        $this->model=$model;
+
+        $this->model_name=is_string($model)?$model:get_class($model);
+        $this->model_name=preg_replace('|^(.*/)?(.*)$|','\1Model_\2',$this->model_name);
+        if(!$this->model)$this->model=$this->add($this->model_name);
+        
         if($display_field)$this->display_field=$display_field;
 
         $this->owner->addExpression($this->getDereferenced())
@@ -15,22 +19,56 @@ class Field_Reference extends Field {
 
         return $this;
     }
-    function ref($load=true){
-        if(is_string($this->model)){
-            $model=preg_replace('|^(.*/)?(.*)$|','\1Model_\2',$this->model);
-            $model=$this->add($model);
-        }else{
-            $model = clone $this->model;
+    /**
+     * ref() will traverse reference and will attempt to load related model's entry. If the entry will fail to load
+     * it will return model which would not be loaded. This can be changed by specifying an argument:
+     *
+     * 'model' - simply create new model and return it without loading anything
+     * false or 'ignore' - will not even try to load anything
+     * null (default) - will tryLoad()
+     * 'load' - will always load the model and if record is not present, will fail
+     * 'create' - if record fails to load, will create new record, save, get ID and insert into $this
+     * 'link' - if record fails to load, will return new record, with appropriate afterSave hander, which will
+     *          update current model also and save it too.
+     *
+     */
+    function ref($mode=null){
+        if($mode=='model'){
+            return $this->add($this->model_name);
         }
 
+        $this->model->unload();
 
-        if(!$load)return $model;
-        if(!$this->get())throw $this->exception('Reference field has no value')
-            ->addMoreInfo('model',$this->owner)
-            ->addMoreInfo('field',$this->short_name)
-            ->addMoreInfo('id',$this->owner->id)
-            ;
-        return $model->load($this->get());
+
+        if($mode===false || $mode=='ignore'){
+            return $this->model;
+        }
+        if($mode=='load'){
+            return $this->model->load($this->get());
+        }
+        if($mode===null){
+            if($this->get())$this->model->tryLoad($this->get());
+            return $this->model;
+        }
+        if($mode=='create'){
+            if($this->get())$this->model->tryLoad($this->get());
+            if(!$this->model->loaded()){
+                $this->model->save();
+                $this->set($this->model->id);
+                $this->owner->update();
+                return $this->model;
+            }
+        }
+        if($mode=='link'){
+            $m=$this->add($this->model_name);
+            if($this->get())$m->tryLoad($this->get());
+            $t=$this;
+            $m->addHook('afterSave',function($m)use($t){
+                $t->set($m->id);
+                $t->owner->save();
+            });
+            return $m;
+        }
     }
     function getDereferenced(){
         if($this->dereferenced_field)return $this->dereferenced_field;
@@ -50,9 +88,9 @@ class Field_Reference extends Field {
             $this->model=$this->add($this->model);
         }
 
-        if($this->display()){
+        if($this->display_field){
             $title=$this->model->dsql()->del('fields');
-            $this->model->getElement($this->display())->updateSelectQuery($title);
+            $this->model->getElement($this->display_field)->updateSelectQuery($title);
         }else{
             $title=$this->model->titleQuery();
         }
