0de805b419c218ca606a03eaa674fa62e2062e8a
Janis Volbergs <j@agiletech.ie>
2012-04-13 11:31:50 +0100

    MySQL supports alias in "update" which are needed for addCondition() in models

--- lib/DB/dsql/mysql.php
+++ lib/DB/dsql/mysql.php
@@ -5,6 +5,10 @@
  * rendering methods which appear differently on your database.
  */
 class DB_dsql_mysql extends DB_dsql {
+    function init(){
+        parent::init();
+        $this->sql_templates['update']="update [table] set [set] [where]";
+    }
     function calc_found_rows(){
         return $this->option('SQL_CALC_FOUND_ROWS');
     }
