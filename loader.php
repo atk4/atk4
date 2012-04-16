4e2dd8e04b8c3a8bbbbb6ea25482ecbee36f89e7
Romans Malinovskis <r@agiletech.ie>
2012-04-16 17:07:12 +0100

    added minimum PHP version check

--- loader.php
+++ loader.php
@@ -1,6 +1,11 @@
 <?php
 // This sets initial path for the include. Further includes will be initialized through PathFinder
 set_include_path('.'.PATH_SEPARATOR.'.'.DIRECTORY_SEPARATOR.'lib'.PATH_SEPARATOR.dirname(__FILE__).'/lib');
+if (version_compare(PHP_VERSION, '5.3.0') < 0) {
+    echo 'PHP 5.3.0 is required';
+    exit;
+}
+
 include 'static.php';
 //include dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'static.php';
 ?>
