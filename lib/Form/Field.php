f83d11c1e3bdc364ef086b9acf733b609606698b
Romans Malinovskis <r@agiletech.ie>
2012-06-14 18:42:11 +0100

    fix form's hidden field

--- lib/Form/Field.php
+++ lib/Form/Field.php
@@ -452,7 +452,7 @@ class Form_Field_Hidden extends Form_Field {
 	}
 	function render(){
         if($this->owner == $this->form){
-            $this->form->template_chunks['form']->append('Content',$this->getInput());
+            $this->form->template_chunks['form']->appendHTML('Content',$this->getInput());
         }else $this->output($this->getInput());
 	}
 }
