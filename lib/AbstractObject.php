e01f44cff8278b6e3395b9f1dbd4c38798b49bab
Romans Malinovskis <r@agiletech.ie>
2012-08-28 01:17:24 +0100

    Fix JavaScript in add-ons
    Wasn't this fixed before?

--- lib/AbstractObject.php
+++ lib/AbstractObject.php
@@ -136,7 +136,7 @@ abstract class AbstractObject {
             $class=$ns.'/'.substr($class,2);
         }
         if (!$short_name)
-            $short_name = strtolower($class);
+            $short_name = str_replace('/','_',strtolower($class));
 
         $short_name=$this->_unique($this->elements,$short_name);
 
