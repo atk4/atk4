2d9419006d2a20398b7203e577c7f3ec1bd828a2
Romans Malinovskis <r@agiletech.ie>
2012-09-21 16:49:54 +0100

    allow field query - simpler querying

--- lib/Model/Table.php
+++ lib/Model/Table.php
@@ -156,6 +156,12 @@ class Model_Table extends Model {
         if($this->title_field && $this->hasElement($this->title_field))return $this->title_field;
         return $this->id_field;
     }
+    /** Retucn query for a specific field. All other fileds are ommitted */
+    function fieldQuery($field){
+        $query=$this->dsql()->del('fields');
+        if(is_string($field))$field=$this->getElement($field);
+        return $field->updateSelectQuery($query);
+    }
     /** Returns query which selects title field */
     function titleQuery(){
         $query=$this->dsql()->del('fields');
