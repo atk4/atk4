8b23cedda2ab407aab20b816e7d554eb573ecb1a
Romans Malinovskis <r@agiletech.ie>
2012-09-29 09:27:16 +0100

    re-use field fieldd's get expr for ordering

--- lib/Model/Table.php
+++ lib/Model/Table.php
@@ -299,14 +299,7 @@ class Model_Table extends Model {
             $this->_dsql()->order($field->getExpr());
             return $this;
         }
-
-        if($field->relation){
-            $this->_dsql()->order($field->relation->short_name.'.'.($field->actual_field?:$field->short_name),$desc);
-        }elseif($this->relations){
-            $this->_dsql()->order(($this->table_alias?:$this->table).'.'.$field->short_name,$desc);
-        }else{
-            $this->_dsql()->order($field->short_name,$desc);
-        }
+        $this->_dsql()->order($field->getExpr(), $desc);
 
         return $this;
     }
