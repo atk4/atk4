cda88610b4c948e284609490766957559e0d2690
Romans Malinovskis <r@agiletech.ie>
2012-11-17 15:52:18 +0000

    fix arrows

--- lib/View/DropButton.php
+++ lib/View/DropButton.php
@@ -1,17 +1,22 @@
 <?php
 class View_DropButton extends Button {
+
+    public $menu=null;
+
     function render(){
         if(!isset($this->options['icons']['secondary'])){
             $this->options['icons']['secondary']='ui-icon-triangle-1-s';
         }
         parent::render();
+        if($this->menu){
+            $this->menu->js(true)->append('<div class="arrow top">ouaoue</div>');
+        }
     }
     /* Show menu when clicked */
     function useMenu($width=3){
-        $m=$this->owner->add('Menu_jUI',null,$this->owner->spot);
+        $this->menu=$m=$this->owner->add('Menu_jUI',null,$this->owner->spot);
         //$m->addStyle('display','none');
 
-        $m->add('View')->addClass('arrow top');
 
         $this->js('click',array(
             $m->js()->show()->position(array(
