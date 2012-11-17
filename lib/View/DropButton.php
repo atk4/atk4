4df688ee6290f00d99a4dc90a11a0aedb0103078
Romans Malinovskis <r@agiletech.ie>
2012-11-17 17:30:39 +0000

    refactor flayouts

--- lib/View/DropButton.php
+++ lib/View/DropButton.php
@@ -8,29 +8,17 @@ class View_DropButton extends Button {
             $this->options['icons']['secondary']='ui-icon-triangle-1-s';
         }
         parent::render();
-        if($this->menu){
-        }
     }
     /* Show menu when clicked */
     function useMenu($width=3){
-        $v=$this->owner->add('View',null,$this->spot)->addClass('flyout');
-        $this->menu=$m=$v->add('Menu_jUI');
-        $m->addStyle('display','none')->addStyle('position','absolute');
-        $v->add('View')->setClass('arrow top');
-        $v->addStyle('border: 1px solid red');
-        //if($width)$v->addClass('span'.$width);
+        $flyout=$this->owner->add('View_Flyout',null,$this->spot);
+        $flyout->useArrow();
+
+        $this->menu=$flyout->add('Menu_jUI');
+
+        $this->js('click',$flyout->showJS($this));
 
 
-        $this->js('click',array(
-            $m->js()->show()->position(array(
-                'my'=>'center top+10',
-                'at'=>'center bottom',
-                'of'=>$this,
-            ),
-            $this->js()->_selectorDocument()->one('click',$m->js()->hide()->_enclose())
-        )));
-
-
-        return $m;
+        return $this->menu;
     }
 }
