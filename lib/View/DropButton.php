3907d373dc6540024777b670299226eaccbbaf36
Romans Malinovskis <r@agiletech.ie>
2012-11-17 17:02:22 +0000

    fixes

--- lib/View/DropButton.php
+++ lib/View/DropButton.php
@@ -9,27 +9,27 @@ class View_DropButton extends Button {
         }
         parent::render();
         if($this->menu){
-            $this->js(true)->_selector('#'.$this->menu->name)->append('<div class="arrow top"></div>');
         }
     }
     /* Show menu when clicked */
     function useMenu($width=3){
-        $this->menu=$m=$this->owner->add('Menu_jUI',null,$this->spot);
-        $this->owner->add('Order')->move($this->menu,'before',$this)->now();
+        $v=$this->owner->add('View',null,$this->spot)->addClass('flyout');
+        $this->menu=$m=$v->add('Menu_jUI');
         $m->addStyle('display','none')->addStyle('position','absolute');
+        $v->add('View')->setClass('arrow top');
+        $v->addStyle('border: 1px solid red');
+        //if($width)$v->addClass('span'.$width);
 
 
         $this->js('click',array(
             $m->js()->show()->position(array(
-                'my'=>'center top',
+                'my'=>'center top+10',
                 'at'=>'center bottom',
                 'of'=>$this,
-                'offset'=>'20px'
             ),
             $this->js()->_selectorDocument()->one('click',$m->js()->hide()->_enclose())
         )));
 
-        if($width)$m->addClass('span'.$width);
 
         return $m;
     }
