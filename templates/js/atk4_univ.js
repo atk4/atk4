ef712b38189e6bfee06b1354e6dbeb64ee4ac92a
Imants Horsts <imants.horsts@inbox.lv>
2012-11-27 01:43:59 +0200

    add setInterval and clearInterval in univ()
    It already had setTimeout, but had no setInterval and clearInterval functions.
    I guess these should be added here too.

--- templates/js/atk4_univ.js
+++ templates/js/atk4_univ.js
@@ -16,6 +16,12 @@ $.each({
 	setTimeout: function(code,delay){
 		setTimeout(code,delay);
 	},
+	setInterval: function(code,delay){
+		setInterval(code,delay);
+	},
+	clearInterval: function(a){
+		clearInterval(a);
+	},
 	displayAlert: function(a){
 		alert(a);
 	},
