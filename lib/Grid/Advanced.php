ec7e2333433ded78dfbd63c911d4e659be419560
Romans Malinovskis <r@agiletech.ie>
2012-12-06 15:29:40 +0000

    update comments and licensing info

--- lib/Grid/Advanced.php
+++ lib/Grid/Advanced.php
@@ -1,4 +1,19 @@
 <?php // vim:ts=4:sw=4:et:fdm=marker
+/*
+ * Undocumented
+ *
+ * @link http://agiletoolkit.org/
+*//*
+==ATK4===================================================
+   This file is part of Agile Toolkit 4
+    http://agiletoolkit.org/
+
+   (c) 2008-2012 Romans Malinovskis <romans@agiletoolkit.org>
+   Distributed under Affero General Public License v3 and
+   commercial license.
+
+   See LICENSE or LICENSE_COM for more information
+ =====================================================ATK4=*/
 /**
  * This is a Basic Grid implementation, which produces fully
  * functional HTML grid capable of filtering, sorting, paginating
