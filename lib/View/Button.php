44939617adba92e0053116d06ba237636653480b
Romans Malinovskis <r@agiletech.ie>
2012-12-06 12:01:06 +0000

    remove prefix from setIcon(),
    used to prefix argument with ui-icon. Now it's not used because in
    some designs a different icon prefixes can be used, for example
    when icons are not coming from jQuery UI. For that reason, you'll need
    to manually prefix your icon names with 'ui-icon' if you are upgrading

--- lib/View/Button.php
+++ lib/View/Button.php
@@ -50,8 +50,8 @@ class View_Button extends View_HtmlElement {
     }
     /** redefine this method with empty one if you DONT want buttons to use jQuery UI */
     function jsButton(){
-        $opt=array();
-        if($this->icon)$opt['icons']['primary']='ui-icon-'.$this->icon;
+        $opt=$this->options;
+        if($this->icon)$opt['icons']['primary']=$this->icon;
         if(!($this->owner instanceof ButtonSet))$this->js(true)->button($opt);
     }
     function render(){
