b832b57058ae77d0d52a40cb8f3f09c72a13dfbc
Romans Malinovskis <r@agiletech.ie>
2012-12-19 09:22:02 +0000

    jqury and jQuery UI version bumps

---
+++ tools/getjq.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export v=$1
+
+[ "$v" ] || { echo "Specify version" ; exit 1; }
+
+rm jquery-$v.min.js
+wget http://code.jquery.com/jquery-$v.min.js
+
+echo "        else(\$v=\$this->api->getConfig('js/jquery','jquery-$v.min'));   // bundled jQuery version" > tmp
+vim -e ../../lib/jQuery.php <<EOF
+/\/\/ bundled jQuery version/
+d
+-
+r tmp
+w
+EOF
+git add jquery-$v.min.js
+
