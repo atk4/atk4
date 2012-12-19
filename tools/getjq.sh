f3d485254bf2724a998a83fa538396fcf33f857d
Romans Malinovskis <r@agiletech.ie>
2012-12-19 09:24:23 +0000

    finalize version bump

--- tools/getjq.sh
+++ tools/getjq.sh
@@ -7,6 +7,7 @@ rm jquery-$v.min.js
 wget http://code.jquery.com/jquery-$v.min.js
 
 echo "        else(\$v=\$this->api->getConfig('js/jquery','jquery-$v.min'));   // bundled jQuery version" > tmp
+trap "rm tmp" EXIT
 vim -e ../../lib/jQuery.php <<EOF
 /\/\/ bundled jQuery version/
 d
