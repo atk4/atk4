e882fa7d2a734fec9ee66858f648b3a0f38e7714
Romans Malinovskis <r@agiletech.ie>
2012-12-21 05:34:06 +0000

    minor fix

--- lib/View/Flyout.php
+++ lib/View/Flyout.php
@@ -14,6 +14,7 @@ class View_Flyout extends View {
         /*
         $this->add('View')->setClass('arrow '.$this->position);
          */
+        return $this;
     }
 
     /* Returns JS which will position this element and show it */
