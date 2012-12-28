72907fe9fa0dc53348915dd8f271b507e0b572bf
Imants Horsts <imants.horsts@inbox.lv>
2012-12-28 02:01:26 +0200

    fix nr.2
    One more fix.
    I guess I'm just to sleepy tonight :)

--- lib/Model/Table.php
+++ lib/Model/Table.php
@@ -405,8 +405,8 @@ class Model_Table extends Model {
     function sum($field){
         if(!is_object($field))$field=$this->getElement($field);
 
-        $q=$this->dsql()
-            ->fieldQuery($q->expr('sum([s_field])')->setCustom('s_field',$field));
+        $q=$this->dsql();
+        $q->fieldQuery($q->expr('sum([s_field])')->setCustom('s_field',$field));
         return $q;
     }
     /** @obsolete same as loaded() - returns if any record is currently loaded. */
