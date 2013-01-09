a27fef0e8570cca10dde73c11bcc40e1cb927efd
Romans Malinovskis <r@agiletech.ie>
2013-01-09 13:17:07 +0000

    Compliance with PSR-2/PEAR

--- lib/AbstractObject.php
+++ lib/AbstractObject.php
@@ -1,20 +1,21 @@
 <?php // vim:ts=4:sw=4:et:fdm=marker
 /**
-   This file is part of Agile Toolkit 4 http://agiletoolkit.org/
+ * A base class for all objects/classes in Agile Toolkit.
+ * Do not directly inherit from this class, instead use one of
+ * AbstractModel, AbstractController or AbstractView
+ * 
+ * @link http://agiletoolkit.org/learn/intro
+*//*
+==ATK4===================================================
+   This file is part of Agile Toolkit 4
+    http://agiletoolkit.org/
 
    (c) 2008-2013 Agile Toolkit Limited <info@agiletoolkit.org>
    Distributed under Affero General Public License v3 and
    commercial license.
 
    See LICENSE or LICENSE_COM for more information
-*/
-/**
- * A base class for all objects/classes in Agile Toolkit.
- * Do not directly inherit from this class, instead use one of
- * AbstractModel, AbstractController or AbstractView
- * 
- * @link http://agiletoolkit.org/learn/intro
- */
+ =====================================================ATK4=*/
 abstract class AbstractObject
 {
     public $settings=array('extension'=>'.html');
