541a88dccc0f551195f4622ea046bc6124deb6ab
Imants Horsts <imants.horsts@inbox.lv>
2013-01-25 17:41:35 +0200

    Form_Field_Time/Date
    1. Form_Field_Time - not complete solution, but at least will not throw exceptions
    2. Form_Field_Date - simplify code, remove obsolete comments

--- lib/Form/Field.php
+++ lib/Form/Field.php
@@ -505,46 +505,41 @@ class Form_Field_Readonly extends Form_Field {
 }
 class Form_Field_Time extends Form_Field {
     function getInput($attr=array()){
-        return parent::getInput(array_merge(array('type'=>'text',
-                        'value'=>format_time($this->value)),$attr));
+        return parent::getInput(array_merge(array(
+                'type'=>'text',
+                'value'=>date($this->api->getConfig('locale/time','H:i:s'),
+                    strtotime($this->value))
+            ),$attr));
     }
 }
 class Form_Field_Date extends Form_Field {
     private $sep = '-';
-    private $is_valid = false;
 
-    /*function getInput($attr=array()){
-      return parent::getInput(array_merge(array('type'=>'text',
-      'value'=>($this->is_valid ? date('Y-m-d', $this->value) : $this->value)),$attr));
-      }*/
     private function invalid(){
         return $this->displayFieldError('Not a valid date');
     }
     function validate(){
         //empty value is ok
         if($this->value==''){
-            $this->is_valid=true;
             return parent::validate();
         }
-        //checking if there are 2 separators
+        //checking if there are exactly 2 separators
         if(substr_count($this->value, $this->sep) != 2){
             $this->invalid();
         }else{
             $c = explode($this->sep, $this->value);
-            //day must go first, month should be second and a year should be last
+            //year should be first, month should be second and a day should be last
             if(strlen($c[0]) != 4 ||
                     $c[1] <= 0 || $c[1] > 12 ||
                     $c[2] <= 0 || $c[2] > 31)
             {
                 $this->invalid();
             }
-            //now attemting to convert to date
-            if(strtotime($this->value)==''){
+            //now attempting to convert to date
+            if(strtotime($this->value)===false){
                 $this->invalid();
             }else{
-                //$this->set(strtotime($this->value));
                 $this->set($this->value);
-                $this->is_valid=true;
             }
         }
         return parent::validate();
