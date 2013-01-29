006cb9cc84873004673d1399f257bab28acb1de4
Imants Horsts <imants.horsts@inbox.lv>
2013-01-29 21:25:08 +0200

    SQL_Many: normalizeClassName

--- lib/SQL/Many.php
+++ lib/SQL/Many.php
@@ -22,10 +22,9 @@ class SQL_Many extends AbstractModel {
     public $auto_track_element=true;
     public $relation=null;
     public $table_alias=null;
+    
     function set($model,$their_field=null,$our_field=null,$relation=null){
-
-        $this->model_name=is_string($model)?$model:get_class($model);
-        $this->model_name=$this->api->normalizeClassName($this->model_name,'Model');
+        $this->model_name=$this->api->normalizeClassName($model,'Model');
 
         if($relation){
             $this->relation=$relation;
