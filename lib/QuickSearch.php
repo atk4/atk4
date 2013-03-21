9e86f2d551de3f3f25caf440e9211745c8cf805b
Imants Horsts <imants.horsts@inbox.lv>
2013-03-22 00:41:00 +0200

    QuickSearch: features & fixes
    1) Implementation of "cancel" button (backwards compatible)
    Can use it if call QuickSearch like this `$grid->addQuickSearch(array('name'),'QuickSearch',array('show_cancel'=>true));`
    2) Buttons moved inside ButtonSet. This allows nicer CSS styling.
    3) You can now also pass comma-separated values (string) of fieldnames when initialize QS.
    Like this: `$grid->addQuickSearch('name,email,phone');`

--- lib/QuickSearch.php
+++ lib/QuickSearch.php
@@ -1,6 +1,7 @@
 <?php
 /***********************************************************
-  ..
+
+  Quicksearch represents one-field filter which goes perfectly with a grid
 
   Reference:
   http://agiletoolkit.org/doc/ref
@@ -16,39 +17,77 @@
    See LICENSE or LICENSE_COM for more information
 =====================================================ATK4=*/
 class QuickSearch extends Filter {
-    /*
-     * Quicksearch represents one-field filter which goes perfectly with a grid
-     */
-    public $icon='ui-icon-search'; // to configure icon
+    
+    // icons
+    public $submit_icon = 'ui-icon-search';
+    public $cancel_icon = 'ui-icon-cancel';
+    
+    // buttonset
+    public $bset_class = 'ButtonSet';
+    public $bset_position = 'after'; // after|before
+    protected $bset;
+    
+    // cancel button
+    public $show_cancel = false; // show cancel button true|false ?
 
     function init(){
         parent::init();
 
+        // template fixes
         $this->addClass('float-right span4 atk-quicksearch');
-        $this->template->trySet('fieldset','atk-row');
+        $this->template->trySet('fieldset', 'atk-row');
         $this->template->tryDel('button_row');
-        $this->search_field=$this->addField('Line','q','')->setNoSave();
-        $this->search_field->addButton('',array('options'=>array('text'=>false)))
-            ->setHtml('&nbsp;')
-            ->setIcon($this->icon)
-            ->js('click',$this->js()->submit());
+        
+        // add field
+        $this->search_field = $this->addField('Line', 'q', '')->setNoSave();
+        
+        // add buttonset
+        if($this->bset_position=='after'){
+            $this->bset = $this->search_field->afterField();
+        }else{
+            $this->bset = $this->search_field->beforeField();
+        }
+        $this->bset = $this->bset->add($this->bset_class);
+
+        // add buttons
+        $this->bset
+            ->addButton('', array('options'=>array('text'=>false)))
+                ->setHtml('&nbsp;')
+                ->setIcon($this->submit_icon)
+                ->js('click', $this->js()->submit());
+        
+        if($this->show_cancel) {
+            $this->bset
+                ->addButton('', array('options'=>array('text'=>false)))
+                    ->setHtml('&nbsp;')
+                    ->setIcon($this->cancel_icon)
+                    ->js('click', array(
+                        $this->search_field->js()->val(null),
+                        $this->js()->submit()
+                    ));
+        }
     }
+    
     function useFields($fields){
-        $this->fields=$fields;
+        if(is_string($fields)) {
+            $fields = explode(',',$fields);
+        }
+        $this->fields = $fields;
         return $this;
     }
+    
     function postInit(){
         parent::postInit();
-        if(!($v=$this->get('q')))return;
+        if(!($v = $this->get('q')))return;
 
         if($this->view->model){
-            $q=$this->view->model->_dsql();
+            $q = $this->view->model->_dsql();
         }else{
-            $q=$this->view->dq;
+            $q = $this->view->dq;
         }
-        $or=$q->orExpr();
+        $or = $q->orExpr();
         foreach($this->fields as $field){
-            $or->where($field,'like','%'.$v.'%');
+            $or->where($field, 'like', '%'.$v.'%');
         }
         $q->having($or);
     }
