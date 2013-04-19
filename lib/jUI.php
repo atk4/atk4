9b449e1bded71e27e87fe9f9e98b75af8de7e733
Romans Malinovskis <r@agiletech.ie>
2013-04-19 11:08:59 +0100

    cleanup

--- lib/jUI.php
+++ lib/jUI.php
@@ -27,7 +27,9 @@ class jUI extends jQuery {
     function init(){
 
         parent::init();
-        if($this->api->jui)throw $this->exception('Do not add jUI twice');
+        if (@$this->api->jui) {
+            throw $this->exception('Do not add jUI twice');
+        }
         $this->api->jui=$this;
 
         $this->addDefaultIncludes();
