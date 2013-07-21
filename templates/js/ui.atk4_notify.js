983f686dd83209c6995477c491ab71a9e603f478
Imants Horsts <imants.horsts@inbox.lv>
2013-07-21 17:29:47 +0300

    ATK_Notify: rewrite, changes and fixes
    Notification widget rewrite, chages and fixes:
    1) Almost complete JS rewrite + nice comments
    2) Fix related LESS and CSS
    3) Most important fix here is close button (x) fixes on notify messages.
    
    +new utility css class .hidden{display:none} This could be useful in
    many templates.

--- templates/js/ui.atk4_notify.js
+++ templates/js/ui.atk4_notify.js
@@ -1,103 +1,139 @@
-/*
+/**
  * ATK Notification UI Widget
- *
-*/
+ */
 
 (function($){
 
 $.widget('ui.atk4_notify', {
 
-	// Few different ways to display a message
-	options: {
-		show: function(){ this.fadeIn() },
-		hide: function(){ this.fadeOut() }
-	},
-	
-	_defineBehaviour: function(message){
-		// When click button is present - use it
-		var self=this;
-		if(message.find('.close').length){
-			message.find('.close').click(function(){
-				message.fadeOut(500,function(){
-					message.remove();
-				});
-			});
-		}else{
-			// otherwise, the whole message disapears on click
-			message.click(function(){
-				message.fadeOut(500,function(){
-					message.remove();
-				});
-			});
-		}
+    /**
+     * Configuration
+     */
+    options: {
+        
+        // Set function here how to show/hide a message
+        show: function(){ this.fadeIn() },
+        hide: function(){ this.fadeOut() },
+        
+        // Timeout in miliseconds
+        min_timeout: 3000,
+        inc_timeout: 25,
+        
+        // Close button
+        closable: true,
+        close_text: 'Hide this message'
+    },
 
-        /*
-		message.mouseenter(function(){
-			console.log('entered');
-			message.stop(true);
-			message.css({'data-delay':1});
-			message.find('.close').show();
-		});
+    /**
+     * Display success message
+     * 
+     * @param string text Message text to show
+     */
+    successMessage: function(text) {
+        this.message(text, true);
+    },
 
-		message.mouseleave(function(){
-			message.find('.close').hide();
-			message.delay(self._getTimeout(message));
-			self.options.hide.call(message);
-			message.hide(0,function(){
-				message.remove();
-			});
-		});
-        */
+    /**
+     * Display error message
+     * 
+     * @param string text Message text to show
+     */
+    errorMessage: function(text) {
+        this.message(text, false);
+    },
 
-	},
-	_insertMessage: function(message){
-		/* 
-		 * Add message into container
-		 */
-		message.hide();
-		this.element.prepend(message);
-		this.options.show.call(message);
-		message.delay(this._getTimeout(message));
-		this.options.hide.call(message);
-		message.hide(0,function(){
-			message.remove();
-		});
-	},
+    /**
+     * Display message
+     * 
+     * @param string text Message text to show
+     * @param boolean success Show success message if true, error message otherwise
+     */
+    message: function(text, success) {
+        var html = $(
+            '<div class="atk-notification ui-state-'+(success?'highlight':'error')+' ui-corner-all">'+
+                '<div class="atk-notification-text">'+
+                    '<i class="ui-icon ui-icon-'+(success?'info':'alert')+'"></i>'+
+                    '<span>'+text+'</span>'+
+                '</div>'+
+                (this.options.closable
+                    ? '<i title="'+this.options.close_text+'" class="ui-icon ui-icon-closethick"></i>'
+                    : ''
+                ) +
+            '</div>');
+        
+        this.messageHTML(html);
+    },
 
-	_getTimeout: function(message){
-		var r=3000+message.text().length*25;
-		return r;
-	},
-	_customiseMessage: function(message){
-		/*
-		 * redefine this to add some custom markup for your messages
-		 */
-		//message.addClass('light-gray');
-	},
-	messageHTML: function(message){
-		this._customiseMessage(message);
-		this._defineBehaviour(message);
-		this._insertMessage(message);
-	},
-	message: function(text,success){
-		/*
-		 * This display a message which you would commonly use on successful operation completion.
-		 */
-        var html=$('<div class="atk-notification ui-state-'+(success?'highlight':'error')+
-        ' ui-corner-all"><div class="atk-notification-text"><i class="ui-icon ui-icon-'+(success?'info':'alert')+'"></i>'
-				+text+'</div><a href="javascript:void()" title="Hide this message" class="ui-icon ui-icon-closethick"></i></div>');
-        html.find('span').text(text);
-        html.find('.ui-icon').addClass('close');
+    /**
+     * Prepare message object and show it
+     * 
+     * @param jQuery message Message to show
+     */
+    messageHTML: function(message) {
+        this._defineBehaviour(message);
+        this._customiseMessage(message);
+        this._insertMessage(message);
+    },
 
-		this.messageHTML(html);
-	},
-	successMessage: function(text){
-		this.message(text,true);
-	},
-	errorMessage: function(text){
-		this.message(text,false);
-	}
-	
+    /**
+     * Define behaviour of message object
+     * 
+     * @param jQuery message Message object
+     */
+    _defineBehaviour: function(message) {
+        var self = this;
+        
+        // When close button is present - use it
+        if (this.options.closable) {
+            message.find('.ui-icon-closethick').click(function() {
+                message.stop();
+                self.options.hide.call(message);
+            });
+        } else {
+            // otherwise, the whole message disappears on click
+            message.click(function() {
+                message.stop();
+                self.options.hide.call(message);
+            });
+        }
+    },
+
+    /**
+     * Redefine this to add some custom markup or actions for your messages
+     * 
+     * @param jQuery message Message object
+     */
+    _customiseMessage: function(message) {
+        // message.addClass('light-gray');
+    },
+
+    /**
+     * Add message object into container
+     * 
+     * @param jQuery message Message object
+     */
+    _insertMessage: function(message) {
+        message.hide();
+        this.element.prepend(message);
+        
+        this.options.show.call(message);
+        message.delay(this._getTimeout(message));
+        this.options.hide.call(message);
+        
+        message.hide(0, function() {
+            message.remove();
+        });
+    },
+
+    /**
+     * Return time in ms for how long we should show message.
+     * 
+     * @param jQuery message Message object
+     */
+    _getTimeout: function(message) {
+        return this.options.min_timeout +
+               message.text().length * this.options.inc_timeout;
+    }
 });
 
 })($);
