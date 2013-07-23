625a54c41b3a8c18c7eee715e179f0725ce8b590
Imants Horsts <imants.horsts@inbox.lv>
2013-07-23 14:42:29 +0300

    PSR-2

--- lib/BaseException.php
+++ lib/BaseException.php
@@ -1,13 +1,13 @@
 <?php // vim:ts=4:sw=4:et:fdm=marker
 /**
-  BaseException is parent of all exceptions in Agile Toolkit which
-  are meant to be for informational purposes. There are also some
-  exceptions (StopInit) which are used for data-flow.
-
-  Learn:
-
-  Reference:
-*//*
+ * BaseException is parent of all exceptions in Agile Toolkit which
+ * are meant to be for informational purposes. There are also some
+ * exceptions (StopInit) which are used for data-flow.
+ *
+ * Learn:
+ *
+ * Reference:
+ *//*
 ==ATK4===================================================
    This file is part of Agile Toolkit 4
     http://agiletoolkit.org/
@@ -18,78 +18,198 @@
 
    See LICENSE or LICENSE_COM for more information
  =====================================================ATK4=*/
-class BaseException extends Exception {
+class BaseException extends Exception
+{
     // Exception defines it's methods as "final", which is complete nonsence
     // and incorrect behavor in my opinion. Therefore I need to re-declare
     // it's class and re-define the methods so I could extend my own methods
     // in my classes.
-    private $frame_stop;
-    public $my_backtrace;
-    public $shift=0;
-    public $name;
-    public $owner=null;
-    public $api=null;
 
-    public $more_info=array();
+    // Backtrace array
+    public $my_backtrace;
+    
+    // Backtrace shift
+    public $shift = 0;
+    
+    // Classname of exception
+    public $name;
+
+    // Array with more info
+    public $more_info = array();
+    
+    // Array of available actions
     public $actions;
-    function init(){
+
+
+
+    /**
+     * Initialization
+     */
+    function init()
+    {
     }
-    function __construct($msg,$code=0){
-        parent::__construct($msg,$code);
+
+    /**
+     * On class construct
+     *
+     * @param string $msg Error message
+     * @param string $code Error code
+     *
+     * @return void
+     */
+    function __construct($msg, $code = 0)
+    {
+        parent::__construct($msg, $code);
         $this->collectBasicData($code);
     }
-    function collectBasicData($code){
-        $this->name=get_class($this);
+    
+    /**
+     * Collect basic data of exception
+     * 
+     * @param string $code Error code
+     *
+     * @return void
+     */
+    function collectBasicData($code)
+    {
+        $this->name = get_class($this);
         $this->my_backtrace = debug_backtrace();
     }
-    /** Call this to add additional information to the exception you are about to throw */
-    function addMoreInfo($key,$value){
-        $this->more_info[$key]=$value;
+    
+    /**
+     * Call this to add additional information to the exception you are about
+     * to throw.
+     * 
+     * @param string $key
+     * @param string $value
+     *
+     * @return $this
+     */
+    function addMoreInfo($key, $value)
+    {
+        $this->more_info[$key] = $value;
         return $this;
     }
-    /** Add reference to the object. Do not call this directly, exception() method takes care of that */
-    function addThis($t){
-        return $this->addMoreInfo('Raised by object',$t);
+    
+    /**
+     * Add reference to the object.
+     * Do not call this directly, exception() method takes care of that.
+     * 
+     * @param string $t
+     */
+    function addThis($t)
+    {
+        return $this->addMoreInfo('Raised by object', $t);
     }
-    /** Actions will be displayed as links on the exception page allowing viewer to perform additional debug
-     * functions. addAction('show info',array('info'=>true)) will result in link to &info=1  */
-    function addAction($key,$descr){
-        $this->actions[$key]=$descr;
+    
+    /**
+     * Actions will be displayed as links on the exception page allowing viewer
+     * to perform additional debug functions.
+     * addAction('show info',array('info'=>true)) will result in link to &info=1
+     *
+     * @param string $key
+     * @param array $descr
+     *
+     * @return $this
+     */
+    function addAction($key, $descr)
+    {
+        $this->actions[$key] = $descr;
         return $this;
     }
-    function getMyTrace(){
+    
+    /**
+     * Return collected backtrace info
+     *
+     * @return array
+     */
+    function getMyTrace()
+    {
         return $this->my_backtrace;
     }
-    function getAdditionalMessage(){
-        return '';
+    
+    /**
+     * Return filename from backtrace log
+     * 
+     * @return string
+     */
+    function getMyFile()
+    {
+        return $this->my_backtrace[2]['file'];
     }
-    function getMyFile(){ return $this->my_backtrace[2]['file']; }
-    function getMyLine(){ return $this->my_backtrace[2]['line']; }
 
-    /** Returns HTML representation of the exception */
-    function getHTML($message=null){
-        $html='';
-        $html.= '<h2>'.get_class($this).(isset($message)?': '.$message:'').'</h2>';
-        $html.= '<p><font color=red>' . $this->getMessage() . '</font></p>';
-        $html.= '<p><font color=blue>' . $this->getMyFile() . ':' . $this->getMyLine() . '</font></p>';
-        $html.=$this->getDetailedHTML();
-        $html.= backtrace($this->shift+1,$this->getMyTrace());
+    /**
+     * Return line number from backtract log
+     *
+     * @return string
+     */
+    function getMyLine()
+    {
+        return $this->my_backtrace[2]['line'];
+    }
+
+    /**
+     * Returns HTML representation of the exception
+     *
+     * @param string $message
+     *
+     * @return string
+     */
+    function getHTML($message = null)
+    {
+        $msg = isset($message) ? ': ' . $message : '';
+        
+        $html =
+            '<h2>' . get_class($this) . $msg . '</h2>' .
+            '<p><font color=red>'  . $this->getMessage() . '</font></p>' .
+            '<p><font color=blue>' . $this->getMyFile() . ':' .
+            $this->getMyLine() . '</font></p>' .
+            $this->getDetailedHTML() .
+            backtrace($this->shift + 1, $this->getMyTrace());
+        
         return $html;
     }
-    /** Returns Textual representation of the exception */
-    function getText(){
-        $text='';$args=array();
-        foreach($this->more_info as $key=>$value){
-            if(is_array($value))$value='Array()';
-            $args[]=$key.'='.$value;
+    
+    /**
+     * Returns Textual representation of the exception
+     *
+     * @return string
+     */
+    function getText()
+    {
+        $text = '';
+        $args = array();
+        foreach ($this->more_info as $key => $value) {
+            if (is_array($value)) {
+                $value = 'Array()';
+            }
+            $args[] = $key . '=' . $value;
         }
 
-        $text.= get_class($this).': '.$this->getMessage().' ('.join(', ',$args).')';
-        $text.= ' in '.$this->getMyFile() . ':' . $this->getMyLine();
+        $text .= get_class($this) . ': ' . $this->getMessage() .
+                 ' (' . join(', ', $args) . ')';
+        $text .= ' in ' . $this->getMyFile() . ':' . $this->getMyLine();
         return $text;
     }
-    /** Redefine this function to add additional HTML output */
-    function getDetailedHTML(){
+
+    /**
+     * Redefine this function to add additional HTML output
+     * 
+     * @return string
+     */
+    function getDetailedHTML()
+    {
+        return '';
+    }
+
+    /**
+     * Undocumented
+     *
+     * @return string
+     * @todo Check this method, looks something useless. Optionally used only in Logger class.
+     */
+    function getAdditionalMessage()
+    {
         return '';
     }
 }
