84351a24045679f1865a5cceaae6277ecea43925
Romans Malinovskis <r@agiletech.ie>
2013-09-07 00:04:35 +0100

    work in progress

--- lib/View/CRUD.php
+++ lib/View/CRUD.php
@@ -377,6 +377,18 @@ class View_CRUD extends View
             );
     }
 
+
+    /** 
+     * Assuming that your model contains a certain method, this allows
+     * you to create a frame which will pop you a new frame with
+     * a form representing model method arguments. Once the form
+     * is submitted, the action will be evaluated
+     */
+    function addAction($method_name, $options = array()) {
+        
+    }
+
+
     /**
      * Transparent method for adding buttons to a crud
      */
