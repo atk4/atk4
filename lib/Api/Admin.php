3047b8c7fa15fd1353a973a9ec6def8f06dd1cd4
Romans Malinovskis <r@agiletech.ie>
2013-10-28 18:51:21 +0000

    Add comments for ApiAdmin

--- lib/Api/Admin.php
+++ lib/Api/Admin.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Api_Admin should be used for building your own application's administration
+ * model. The benefit is that you'll have access to a number of add-ons which
+ * are specifically written for admin system.
+ *
+ * Exporting add-ons, database migration, test-suites and other add-ons
+ * have developed User Interface which can be simply "attached" to your
+ * application's admin.
+ *
+ * This is done through hooks in the Admin Class. It's also important that
+ * Api_Admin relies on layout_fluid which makes it easier for add-ons to
+ * add menu items, sidebars and foot-bars.
+ */
 class Api_Admin extends ApiFrontend {
 
     public $menu;
