af4a65f369a9ae1aef93e810e13666dd2f92a5e5
Janis Volbergs <j@agiletech.ie>
2013-12-19 12:33:57 +0200

    Fix in paginator - position was not memorized properly

--- lib/Paginator/Basic.php
+++ lib/Paginator/Basic.php
@@ -41,7 +41,11 @@ class Paginator_Basic extends CompleteLister {
     /** Set a custom source. Must be an object with foundRows() method */
     function setSource($source){
         if($this->memorize){
-            $this->skip=$this->memorize('skip', @$_GET[$this->skip_var])+0;
+            if (isset($_GET[$this->skip_var])){
+                $this->skip=$this->memorize('skip', @$_GET[$this->skip_var])+0;
+            } else {
+                $this->skip=$this->recall('skip',0);
+            }
         }else{
             $this->skip=@$_GET[$this->skip_var]+0;
         }
