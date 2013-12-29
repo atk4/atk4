7fd67734934540071e8d935900b83fd6a28de712
Romans Malinovskis <r@agiletech.ie>
2013-12-29 22:14:22 +0200

    js lint

--- public/atk4/js/start-atk4.js
+++ public/atk4/js/start-atk4.js
@@ -13,7 +13,7 @@
   $this->js(true)->_load('start-atk4');
 */
 
-;jQuery.atk4||(function($){
+jQuery.atk4||(function($){
 
 /*
 
@@ -41,7 +41,6 @@ $.atk4 = function(readycheck,lastcall){
  can optionally extend it by adding your own functions
 */
 $.univ = function(){
-	var ignore=true;
 	$.univ.ignore=false;
 	return $.univ;
 };
@@ -52,11 +51,11 @@ $.univ._import=function(name,fn){
 			ret=fn.apply($.univ,arguments);
 		}
 		return ret?ret:$.univ;
-	}
-}
+	};
+};
 $.fn.extend({
     univ: function(){
-        var u=new $.univ;
+        var u=new $.univ();
         u.jquery=this;
         return u;
     }
@@ -78,51 +77,30 @@ $.extend($.atk4,{
 	///////////////////////// ERROR HANDLING //////////////////////////
 
 	/*
-	 We store the knowledge whether this is production environment
-	 or not. We also provide a way how user will be notified about
-	 critical JS errors
-	*/
-	/*
-	 Something has gone wrong. We should display error to the user.
-	*/
-	errorMessage: function(msg,moreinfo){
-		if(this.production){
-			console.log('Surpressed user error in production environment',text);
-			return;
-		}
+       This function is used to display success messages. This should probably be
+       redefined and should make appear pretty for the user
 
-        w=window.open(null,null,'height=400,width=700,location=no,menubar=no,scrollbars=yes,status=no,titlebar=no,toolbar=no');
-        if(w){
-            w.document.write('<h2>JavaScript Error: '+e+'</h2>');
-            w.document.write(response_text);
-            w.document.write('<center><input type=button onclick="window.close()" value="Close"></center>');
-        }else{
-            showMessage("JavaScript Error: "+e+"\n"+response_text);
-        }
-	},
-	/*
-	 This function is used to display success messages. This should probably be
-	 redefined and should make appear pretty for the user
-
-	 Set 2nd argument to true, if it's a system message, which most likely will
-	 not be interesting to the user on production environment
+       Set 2nd argument to true, if it's a system message, which most likely will
+       not be interesting to the user on production environment
 	*/
 	successMessage: function(msg,system){
-		if(system && this.production)return;
-		console.log('Success: ',msg);
+		if(system && this.production) {
+            return;
+        }
+		window.console.log('Success: ',msg);
 	},
 
 
 	//////////////////////////////// AJAX ///////////////////////////////////
 	/*
-	 This is lowest-level AJAX function. It will attepmt to get document from
-	 the server and perform some very basic validation. For instance it will
-	 check if server's session is expired or if it returns error.
+       This is lowest-level AJAX function. It will attepmt to get document from
+       the server and perform some very basic validation. For instance it will
+       check if server's session is expired or if it returns error.
 
-	 Other modules of ATK4 should rely on this function. If you want to
-	 load your own AJAX, use $('..').atk4_load() instead.
+       Other modules of ATK4 should rely on this function. If you want to
+       load your own AJAX, use $('..').atk4_load() instead.
 
-	 NOTE: get does not assume regarding the type of returned data.
+       NOTE: get does not assume regarding the type of returned data.
 	*/
     loading: 0,	// How many files are currently being requested
 
@@ -138,8 +116,12 @@ $.extend($.atk4,{
 	// Server-side implements session timeout
 	_refreshTimeout: function(){
 		if(document.session_timeout){
-			if(document.session_timeout_timer1)clearTimeout(document.session_timeout_timer1);
-			if(document.session_timeout_timer2)clearTimeout(document.session_timeout_timer2);
+			if(document.session_timeout_timer1){
+                clearTimeout(document.session_timeout_timer1);
+            }
+			if(document.session_timeout_timer2){
+                clearTimeout(document.session_timeout_timer2);
+            }
 
 			document.session_timeout_timer1=setTimeout(function(){
 				if($.univ && $.univ().successMessage){
@@ -150,9 +132,10 @@ $.extend($.atk4,{
 
 			document.session_timeout_timer2=setTimeout(function(){
 				if($.univ()){
-					$.univ().dialogOK('Session timeout','You have been inactive for '+document.session_timeout+' minutes. You will need to log-in again',function(){ document.location='/' });
+					$.univ().dialogOK('Session timeout','You have been inactive for '+document.session_timeout+' minutes. You will need to log-in again',function(){ document.location='/'; });
 				}else{
-					alert('Your session have expired');document.location='/';
+					window.alert('Your session have expired');
+                    document.location='/';
 				}
 			},(document.session_timeout)*60*1000);
 		}
@@ -170,8 +153,12 @@ $.extend($.atk4,{
 		var timeout=setTimeout(function(){
 			self._stillLoading(url);
 		},2000);
-		if(typeof(url)=="object" && url[0])url=$.atk4.addArgument(url);
-		if(typeof(url)=="string")url={url:url};
+		if(typeof(url)==="object" && url[0]){
+            url=$.atk4.addArgument(url);
+        }
+		if(typeof(url)==="string"){
+            url={url:url};
+        }
 
 		// Another file is being loaded.
         this.loading++;
@@ -185,16 +172,26 @@ $.extend($.atk4,{
             success: function(res){
 					clearTimeout(timeout);
 					$.atk4._refreshTimeout();
-					load_end_callback && load_end_callback();
-					$.atk4._checkSession(res) && callback && callback(res);
-		            if(!--$.atk4.loading)$.atk4._readyExec();
+					if(load_end_callback) {
+                        load_end_callback();
+                    }
+					if($.atk4._checkSession(res) && callback) {
+                        callback(res);
+                    }
+                    if(!--$.atk4.loading){
+                        $.atk4._readyExec();
+                    }
 				},
             error: function(a,b,c){
 					clearTimeout(timeout);
 					$.atk4._refreshTimeout();
-					load_end_callback && load_end_callback();
+					if(load_end_callback) {
+                        load_end_callback();
+                    }
 					$.atk4._ajaxError(url,a,b,c);
-		            if(!--$.atk4.loading)$.atk4._readyExec();
+                    if(!--$.atk4.loading){
+                        $.atk4._readyExec();
+                    }
 					// kill readycheck handlers by not reducing
 					// the counter.
 				}
@@ -202,37 +199,37 @@ $.extend($.atk4,{
     },
 	_stillLoading: function(url){
 		if(this.loading){
-			console.log('Slow loading of: ',url,'remaining:',this.loading);
+			window.console.log('Slow loading of: ',url,'remaining:',this.loading);
 		}
 	},
 	/*
-	 Use $.atk4.prototype to redeclare below 2 functions to your liking.
+       Use $.atk4.prototype to redeclare below 2 functions to your liking.
 	*/
     _ajaxError: function(url,a,b,c){
-		console.error("Failed to load file: ",url," (",a,b,c,")");
+		window.console.error("Failed to load file: ",url," (",a,b,c,")");
     },
     _checkSession: function(text){
 		// TODO: use proper session handling instead
-        if(text.substr(0,7)=="ERROR: "){
+        if(text.substr(0,7)==="ERROR: "){
 			var msg=text.substr(7);
-			alert(msg);
+			window.alert(msg);
 			return false;
 		}
-        if($.trim(text)=="SESSION TIMEOUT"){
-            alert('session has timed out');
+        if($.trim(text)==="SESSION TIMEOUT"){
+            window.alert('session has timed out');
             document.location="/";
             return false;
         }
         return true;
     },
 	/*
-	 queues function to be executed when loading are complete.
-	 If "lastcall" is specified as true, then function will be
-	 executed after everything else. Only one function can be
-	 specified as lastCall.
+       queues function to be executed when loading are complete.
+       If "lastcall" is specified as true, then function will be
+       executed after everything else. Only one function can be
+       specified as lastCall.
 
-	 If nothing is being loaded, then functions are executed
-	 immediatelly
+       If nothing is being loaded, then functions are executed
+       immediatelly
 	*/
     _onReady: function(fn,lastcall){
 		if(lastcall){
@@ -245,7 +242,7 @@ $.extend($.atk4,{
 					this._readyLast=function(){
 						prev();
 						fn();
-					}
+					};
 				}else{
 					this._readyLast=fn;
 				}
@@ -258,20 +255,22 @@ $.extend($.atk4,{
             this._readyList.push(fn);
         }
     },
-	/*
-	 if _onReady functions were not executed immediatelly, then
-	 this function will be called at the end and will execute them
-	 all in order. If any of the functions will start loading
-	 more files, execution will terminate and will be resumed
-	 after all files are loaded again.
-	*/
+    /*
+       if _onReady functions were not executed immediatelly, then
+       this function will be called at the end and will execute them
+       all in order. If any of the functions will start loading
+       more files, execution will terminate and will be resumed
+       after all files are loaded again.
+    */
     _readyExec: function(){
         while(this._readyList.length){
-            fn=this._readyList.shift();
+            var fn=this._readyList.shift();
             fn.call(document);
 
 			// We are loading more data, resume after
-			if($.atk4.loading)return;
+			if($.atk4.loading){
+                return;
+            }
         }
 		if(this._readyLast){
 			var x=this._readyLast;
@@ -283,8 +282,8 @@ $.extend($.atk4,{
 	//////////////////// Dynamic Includes (CSS and JS) ////////////////
 
 	/*
-	 Based on get() we add number of functions to dynamically load
-	 JS and CSS files.
+       Based on get() we add number of functions to dynamically load
+       JS and CSS files.
 	*/
 
 
@@ -296,7 +295,9 @@ $.extend($.atk4,{
     includeJS: function(url,nocache){
 
 		// Perhaps file is already included. We do not to load it twice
-        if(this._isIncluded(url) && !nocache)return;
+        if(this._isIncluded(url) && !nocache){
+            return;
+        }
 
 		// Continue with loading
         this.get(url,function(code){
@@ -308,12 +309,14 @@ $.extend($.atk4,{
 				// through browser JS parser
 				if(String(e).indexOf("Parse error")){
 					if($.atk4.production){
-						console.log("Eval failed for "+url);
+						window.console.log("Eval failed for "+url);
 					}else{
-						console.error("Eval failed for "+url+", trying to include directly for debugging");
-	                   $.atk4._evalJS(url)
+                        window.console.error("Eval failed for "+url+", trying to include directly for debugging");
+                        $.atk4._evalJS(url);
 					}
-                }else console.error("Eval error: "+e);
+                }else {
+                    window.console.error("Eval error: "+e);
+                }
             }
         });
     },
@@ -321,9 +324,8 @@ $.extend($.atk4,{
     _evalJS: function(url,clean) {
 		// remove previously evaled piece of code
         var old = document.getElementById('atk4_eval_clean');
-        if (old != null) {
+        if (old !== null) {
             old.parentNode.removeChild(old);
-            delete old;
         }
         var head = document.getElementsByTagName("head")[0];
         var script = document.createElement('script');
@@ -335,16 +337,17 @@ $.extend($.atk4,{
         head.appendChild(script);
     },
 	/*
-	 This function will dynamically load CSS file.
-	 Also relative URLs like url(../images) will not break.
+       This function will dynamically load CSS file.
+       Also relative URLs like url(../images) will not break.
 	*/
     includeCSS: function(url){
-        if(this._isIncluded(url))return;
-
+        if(this._isIncluded(url)){
+            return;
+        }
         /*
-         Dynamically loads CSS. Now works for IE too as noted in:
-         http://stackoverflow.com/questions/1184950/dynamically-loading-css-stylesheet-doesnt-work-on-ie
-         see comment by ekerner on Apr 4 11 at 16:44
+           Dynamically loads CSS. Now works for IE too as noted in:
+           http://stackoverflow.com/questions/1184950/dynamically-loading-css-stylesheet-doesnt-work-on-ie
+           see comment by ekerner on Apr 4 11 at 16:44
         */
         $("<link>")
             .appendTo($('head'))
@@ -352,7 +355,9 @@ $.extend($.atk4,{
             .attr('href', url);
     },
     _isIncluded: function(url){
-        if(this._includes[url])return true;
+        if(this._includes[url]){
+            return true;
+        }
         this._includes[url]=true;
         return false;
     },
@@ -360,13 +365,13 @@ $.extend($.atk4,{
 	//////////////////////////// MISC //////////////////////////////////
 
 	/*
-	 Utility function. When you give it an URL, and argument, it will
-	 append argument to the URL.
+       Utility function. When you give it an URL, and argument, it will
+       append argument to the URL.
 
-	 TODO: this function is incomplete. It should also check if argument is
-	 already in the URL and handle that properly.
+       TODO: this function is incomplete. It should also check if argument is
+       already in the URL and handle that properly.
 
-	 See also: http://api.jquery.com/jQuery.param/
+       See also: http://api.jquery.com/jQuery.param/
 	*/
 	addArgument: function(url,a,b){
 		if(typeof(url)=='object'){
