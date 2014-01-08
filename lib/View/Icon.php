d14d9bc9e2d14bbe247195ef6d0675d53bbb75c8
Romans Malinovskis <r@agiletech.ie>
2014-01-08 19:59:36 +0000

    Icon integrated with fonticons.

--- lib/View/Icon.php
+++ lib/View/Icon.php
@@ -15,29 +15,19 @@
 
    See LICENSE or LICENSE_COM for more information
 =====================================================ATK4=*/
-class View_Icon extends View_HtmlElement {
-    public $color=null;
+class View_Icon extends View {
+    public $shappe=null;
     function init(){
         parent::init();
         $this->setElement('i');
-        parent::set('');
-        $this->setColor($this->api->getConfig('icon/default-color','orange'));
     }
     /* Sets icon by its shape. http://agiletoolkit.org/ref/icon */
     function set($shape){
         $this->shape=$shape;
         return $this;
     }
-    /* CSS may support only few colors. */
-    function setColor($color){
-        $this->color=$color;
-        return $this;
-    }
     function render(){
-        $this->addClass('atk-icon');
-        $this->addClass('atk-icons-'.$this->color);
-        $this->addClass('atk-icon-'.$this->shape);
-
+        $this->addClass('icon-'.$this->shape);
         parent::render();
     }
 
