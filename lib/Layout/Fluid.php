781e4c0cde9444273ed04787e1ac7ba2a508068c
Romans Malinovskis <r@agiletech.ie>
2014-02-19 04:34:00 +0000

    Update fluid layout

--- lib/Layout/Fluid.php
+++ lib/Layout/Fluid.php
@@ -9,11 +9,30 @@
  *
  */
 class Layout_Fluid extends Layout_Basic {
+
+    public $footer_wrap;
+    public $header_wrap;
+
     function defaultTemplate() {
         return array('layout/fluid');
     }
 
+    function addHeader($class = 'Menu_Objective') {
+        $this->header_wrap = $this->add('View',null,'Header',array('layout/fluid','Header'));
+
+        $res=$this->header_wrap->add($class,null,'Header_Content');
+
+        return $res;
+    }
+
     function addMenu($class = 'Menu_Vertical') {
         return $this->add($class,null,'Main_Menu');
+    }
+
+    function addFooter($class = 'View') {
+        $this->footer_wrap = $this->add('View',null,'Footer',array('layout/fluid','Footer'));
+
+        return $this->footer_wrap->add($class,null,'Footer_Content')
+            ->set('This site is developed with Agile Toolkit');
     }
 }
