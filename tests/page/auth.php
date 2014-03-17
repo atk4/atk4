7b97da192658af4b08e34039106984f31a248265
Romans Malinovskis <r@agiletech.ie>
2014-03-17 16:41:56 +0000

    added an ApiCLI so that we can test API inside that.

--- tests/page/auth.php
+++ tests/page/auth.php
@@ -17,38 +17,43 @@ class Model_Auth_User extends Model {
 }
 
 class page_auth extends Page_Tester {
+    public $myapp;
+    function init() {
+        $this->myapp=$this->add('ApiCLI');
+        return parent::init();
+    }
     function test_auth(){
-        $a=$this->add('Auth');
+        $a=$this->myapp->add('Auth');
 
         $a->allow('john','smith');
         return $a->verifyCredentials('john','smith');
     }
     function test_auth2(){
-        $a=$this->add('Auth');
+        $a=$this->myapp->add('Auth');
 
         $a->allow('john','smith');
         return $a->verifyCredentials('john','doe');
     }
     function test_auth3(){
-        $a=$this->add('Auth');
+        $a=$this->myapp->add('Auth');
 
         $a->setModel('Auth_User','myusername','mypassword');
         return(boolean)$a->verifyCredentials('john','doe');
     }
     function test_auth4(){
-        $a=$this->add('Auth');
+        $a=$this->myapp->add('Auth');
 
         $a->setModel('Auth_User','myusername','mypassword');
         return (boolean)$a->verifyCredentials('john','smith');
     }
     function test_auth5(){
-        $a=$this->add('Auth');
+        $a=$this->myapp->add('Auth');
 
         $a->setModel('Auth_User','myusername','mypassword');
         return $a->verifyCredentials('peter','smith');
     }
     function test_auth6(){
-        $a=$this->add('Auth');
+        $a=$this->myapp->add('Auth');
 
         $a->setModel('Auth_User','myusername','mypassword');
         return $a->verifyCredentials('nouser','smith');
