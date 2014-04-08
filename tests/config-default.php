f8e54df4387432b380123ac09b96cf9b0b8bb025
Romans Malinovskis <romans@agiletoolkit.org>
2014-04-08 11:53:43 +0100

    fixing test ui

--- tests/config-default.php
+++ tests/config-default.php
@@ -1,9 +1,9 @@
 <?php
-$config['atk']['base_path']='../';
+#$config['atk']['base_path']='../public/';
 $config['url_postfix']='';
 $config['url_prefix']='?page=';
-$config['logger']['log_output']='full';
+$config['logger']['log_output']=false;
 $config['mongo']['db']='test';
 
 # Logger log directory
-$config['logger']['log_dir']=__DIR__ . '/log';
+#$config['logger']['log_dir']=__DIR__ . '/log';
