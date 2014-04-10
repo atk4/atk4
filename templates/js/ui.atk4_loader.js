783f6dd08de045d38be13bf9dcde4e1f5f4769cc
Imants Horsts <imants.horsts@inbox.lv>
2014-04-10 19:48:20 +0300

    Update ui.atk4_loader.js
    basically rollback

--- templates/js/ui.atk4_loader.js
+++ templates/js/ui.atk4_loader.js
@@ -322,28 +322,14 @@ $.fn.extend({
     atk4_load: function(url,fn){
         this.atk4_loader().atk4_loader('loadURL',url,fn);
     },
-    atk4_reload: function(url,arg,fn,interval){
+    atk4_reload: function(url,arg,fn){
         if(arg){
             $.each(arg,function(key,value){
                 url=$.atk4.addArgument(url,key+'='+encodeURIComponent(value));
             });
         }
 
-        if(interval) {
-            // if interval is set, then do periodic reloads / refreshes
-            // execute reload after defined period of time
-            var id = $.univ.setTimeout(function(obj){
-                obj.atk4_loader()
-                    .atk4_loader('loadURL',url,fn,true);
-            }, interval, this);
-            // if associated DOM element is destroyed, then remove timeout action
-            this.on('remove',function(){
-                $.univ.clearTimeout(id);
-            });
-        } else {
-            // if no interval set, then do simple reloading of element
-            this.atk4_loader()
-                .atk4_loader('loadURL',url,fn,true);
-        }
+        this.atk4_loader()
+            .atk4_loader('loadURL',url,fn,true);
     }
 });
