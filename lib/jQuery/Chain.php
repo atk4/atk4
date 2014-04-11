86e6e4f8ba38cf3607d7dd24650406baa828cc66
Imants Horsts <imants.horsts@inbox.lv>
2014-04-11 13:50:42 +0300

    Periodic View reload functionality

--- lib/jQuery/Chain.php
+++ lib/jQuery/Chain.php
@@ -213,17 +213,43 @@ class jQuery_Chain extends AbstractModel {
         }
         return $s;
     }
-    /* Calls real redirect (from univ), but accepts page name. Use url() for 1st argument manually anyway. */
-    function redirect($page=null,$arg=null){
-        $url=$this->api->url($page,$arg);
-        return $this->_fn('redirect',array($url));
+    /**
+     * Calls real redirect (from univ), but accepts page name
+     *
+     * Use url() for 1st argument manually anyway.
+     * 
+     * @param string $page Page name
+     * @param Array $arg Arguments
+     * 
+     * @return this
+     */
+    function redirect($page = null, $arg = null) {
+        $url = $this->api->url($page, $arg);
+        return $this->univ()->_fn('redirect', array($url));
     }
-    /* Reload object. You can bind this to custom event and trigger it if object is not directly accessible. */
-    function reload($arguments=array(),$fn=null,$url=null){
-        if($fn)$fn->_enclose();
-        $id=$this->owner;
-        if(!$url)$url=$this->api->url(null,array('cut_object'=>$id->name));
-        return $this->_fn('atk4_reload',array($url,$arguments,$fn));
+    /**
+     * Reload object
+     *
+     * You can bind this to custom event and trigger it if object is not
+     * directly accessible.
+     * If interval is given, then object will periodically reload itself.
+     * 
+     * @param Array $arg
+     * @param jQuery_Chain $fn
+     * @param string $url
+     * @param integer $interval Interval in milisec. how often to reload object
+     * 
+     * @return this
+     */
+    function reload($arg = array(), $fn = null, $url = null, $interval = null) {
+        if ($fn && $fn instanceof jQuery_Chain) {
+            $fn->_enclose();
+        }
+        $obj = $this->owner;
+        if (!$url) {
+            $url = $this->api->url(null, array('cut_object' => $obj->name));
+        }
+        return $this->univ()->_fn('reload', array($url, $arg, $fn, $interval));
     }
     /* Chain will not be called but will return callable function instead. */
     function _enclose($fn=null,$preventDefault=false){
