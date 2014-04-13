b91b9b7448fa6d1bc7cdf242e8a451e7742d5884
Romans Malinovskis <romans@agiletoolkit.org>
2014-04-13 11:52:48 +0100

    experimenting with standard-compliant header comments.

--- lib/HR.php
+++ lib/HR.php
@@ -1,24 +1,32 @@
 <?php
-/***********************************************************
-  Add HTML horizontal-ruler <hr> element
+/**
+ * hello
+ *
+ * @category UI
+ * @package  AgileToolkit
+ * @author   Romans Malinovskis <romans@agiletoolkit.org>
+ * @license  AGPL or Commercial
+ * @link     http://agiletoolkit.org/
+ */
 
-  Reference:
-  http://agiletoolkit.org/doc/ref
 
-==ATK4===================================================
-   This file is part of Agile Toolkit 4
-    http://agiletoolkit.org/
-
-   (c) 2008-2013 Agile Toolkit Limited <info@agiletoolkit.org>
-   Distributed under Affero General Public License v3 and
-   commercial license.
-
-   See LICENSE or LICENSE_COM for more information
-=====================================================ATK4=*/
+/**
+ * Add HTML horizontal-ruler <hr> element
+ *
+ * @uses     HtmlElement
+ * @category UI
+ * @package  AgileToolkit
+ * @author   Romans Malinovskis <romans@agiletoolkit.org>
+ * @license  AGPL http://agiletoolkit.org/license
+ * @link     http://agiletoolkit.org/
+ */
 class HR extends HtmlElement
 {
     /**
-     * Initialization
+     * HR places a
+     *
+     * @access public
+     * @return void
      */
     function init()
     {
