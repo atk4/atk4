f7ded315c812ce4e5fdbf689c09ae94a598cffd8
Gowrav Vishwakarma <gowrav@xserver.(none)>
2014-04-17 10:59:54 +0530

    Composer Json File Corrected, Installer created for 4.3

--- public/index.php
+++ public/index.php
@@ -1,8 +1,12 @@
 <?php
 
 chdir('..');
-include 'loader.php'; 
-include 'lib/Frontend.php';
-	
+// Un Comment following line if using with composer
+include 'vendor/autoload.php';
+
+// Un Comment the following line if not using composer
+// include 'loader.php'; 
+
+include 'lib/Frontend.php';	
 $api = new Frontend('myrealm');
 $api->main(); 
