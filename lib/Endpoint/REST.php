20bd94916e70f0fddbcc3be61c20cf1559fc2413
Romans Malinovskis <romans@agiletoolkit.org>
2014-05-02 15:43:08 +0100

    add support for RESTful API servers

---
+++ lib/Endpoint/REST.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Implementation of RESTful endpoint for App_REST
+ */
+class Endpoint_REST {
+
+    public $model_class=null;
+    public $user_id_field='user_id';
+    public $user=null;  // authenticated user
+    public $authenticate=true;
+
+    public $allow_list=true;
+    public $allow_list_one=true;
+    public $allow_add=false;
+    public $allow_edit=false;
+    public $allow_delete=false;
+
+
+    public function _authenticate() {
+        // Verifies user authentication data
+        if(!$this->authenticate)return;
+
+        if($t=$_GET['token']){
+            $guid= $this->_hubid_auth($t);
+            $m=$this->api->add('Model_User')->loadBy('hub_guid',$guid);
+            return $this->user = $m;
+
+        }elseif ($u=$_GET['username']) {
+            $p=$_GET['password'];
+        }else{
+
+            if (!isset($_SERVER['PHP_AUTH_USER'])) {
+
+                //throw $this->exception('Use Authenticaiton');
+
+                header('WWW-Authenticate: Basic realm="VEN Api"');
+                header('HTTP/1.0 401 Unauthorized');
+                echo 'Please use authentication authentication';
+                exit;
+            }
+
+	    $u=$_SERVER['PHP_AUTH_USER'];
+	    $p=$_SERVER['PHP_AUTH_PW'];
+
+        }
+
+        if($_SERVER['PHP_X_VEN_AUTH']) {
+            throw $this->exception('Not Implemented');
+        }
+
+        $auth=$this->api->add('Controller_VenAuth');
+        $result = $auth->verifyCredentials($u,$p);
+
+        if($result === false){
+            //throw $this->exception('Authentication wrong');
+
+            header('WWW-Authenticate: Basic realm="VEN Api"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo 'Authenticaiton wrong';
+            exit;
+        }
+
+        $this->user = $auth->model;
+    }
+
+    protected function _model(){
+        // Based od authentication data, return a valid model
+        if(!$this->model_class)throw $this->exception('Must have model');
+
+        $m=$this->api->add('Model_'.$this->model_class);
+        if($this->user_id_field && $this->authenticate){
+            // if not authenticated, blow up
+            $m->addCondition($this->user_id_field,$this->user->id);
+        }
+
+        $id=$_GET['id'];
+        if(!is_null($id)){
+            $m->load($id);
+        }
+
+        return $m;
+
+    }
+
+    protected function _outputOne($data) {
+
+        if($data['_id']) {
+            $data = array('id'=>(string)$data['_id'])+$data;
+        }
+        unset($data['_id']);
+
+        foreach($data as $key=>$val){
+            if($val instanceof MongoID) {
+                $data[$key]=(string)$val;
+            }
+        }
+
+        return $data;
+    }
+
+    protected function _outputMany($data) {
+        $output = array();
+        foreach($data as $row) {
+            $output[] = $this->_outputOne($row);
+        }
+        return $output;
+    }
+
+    protected function _input($data,$filter=true){
+        // validates input
+        if(is_array($filter)) {
+            $data = array_intersect_key($my_array, array_flip($allowed));
+        }
+
+        unset($data['id']);
+        unset($data['_id']);
+        unset($data['user_id']);
+        unset($data['user']);
+
+        return $data;
+    }
+
+
+    function get(){
+        $m=$this->_model();
+        if($m->loaded()){
+            if(!$this->allow_list_one)throw $this->exception('Loading is not allowed');
+            return $this->_outputOne($m->get());
+        }
+
+        if(!$this->allow_list)throw $this->exception('Listing is not allowed');
+        return $this->_outputMany($m->setLimit(100)->getRows());
+    }
+
+    function save($data){
+        $m=$this->_model();
+
+        if(!$m->loaded()) {
+            if(!$this->allow_add)throw $this->exception('Adding is not allowed');
+
+            $data=$this->_input($data,$this->allow_add);
+
+        }else{
+            if(!$this->allow_edit)throw $this->exception('Editing is not allowed');
+
+            $data=$this->_input($data,$this->allow_edit);
+        }
+
+        return $this->_outputOne($m->set($data)->save()->get());
+    }
+
+    function delete(){
+        if(!$this->allow_delete)throw $this->exception('Deleting is not allowed');
+
+        $m=$this->_model();
+        if(!$m->loaded())throw $this->exception('Cowardly refusing to delete all records');
+
+        $m->delete();
+
+        return true;
+    }
+}
