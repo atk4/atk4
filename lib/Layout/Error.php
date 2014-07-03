697f5aa4be1e583e0c980d389fe35352d9f69d21
Romans Malinovskis <romans@agiletoolkit.org>
2014-07-03 15:53:25 +0100

    Add simple layout for error outputting.

---
+++ lib/Layout/Error.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Centered layout for showing a single form or message on the page
+ */
+class Layout_Error extends Layout_Basic {
+
+
+    function defaultTemplate() {
+        return array('layout/error');
+    }
+
+}
