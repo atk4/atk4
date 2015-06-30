atk4/atk4

commit 103d6d8a65b8666db3c7af767d8dd254c04c1666

Model is much more reliable, works with system fields.

lib/Grid/Advanced.php
@@ -1343,7 +1343,7 @@ class Grid_Advanced extends Grid_Basic
         $this->current_row['_link'] =
             $this->api->url($page, array($attr =>
                 $this->columns[$field]['id_value']
-                ? $this->current_row[$this->columns[$field]['id_value'].'_original']
+                ? $this->model[$this->columns[$field]['id_value']]
                 : $this->current_id));
 
         if (!$this->current_row[$field]) {

commit c58be63db0b1f6b0901980be57b7005860f1bfe6

Fix bug introduced in 103d6d8 (id_value sourced from model)

lib/Grid/Advanced.php
@@ -1343,7 +1343,7 @@ class Grid_Advanced extends Grid_Basic
         $this->current_row['_link'] =
             $this->api->url($page, array($attr =>
                 $this->columns[$field]['id_value']
-                ? $this->model[$this->columns[$field]['id_value']]
+                ? ($this->model[$this->columns[$field]['id_value']]?:$this->current_row[$this->columns[$field]['id_value'].'_original'])
                 : $this->current_id));
 
         if (!$this->current_row[$field]) {