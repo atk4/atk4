96f75ea9817868c59a8ca6c70dd48e4a13ea52ed
Romans Malinovskis <romans@agiletoolkit.org>
2015-09-07 09:09:04 +0100

    implemented search

--- lib/Controller/Data/Clusterpoint.php
+++ lib/Controller/Data/Clusterpoint.php
@@ -4,11 +4,17 @@ class Controller_Data_Clusterpoint extends Controller_Data {
     public $supportLimit = true;
     public $supportOrder = true;
     public $supportRef = true;
-    public $supportOperators = array(); //'=' => true, '>' => true, '>=' => true, '<=' => true, '<' => true, '!=' => true, 'like' => true);
+    public $supportOperators = 'all'; //'=' => true, '>' => true, '>=' => true, '<=' => true, '<' => true, '!=' => true, 'like' => true);
 
     public $transport_class = 'PestJSON';
 
+    function &d($model){
+        return $model->_table[$this->short_name];
+    }
 
+    /**
+     * This method is called by a model to initialize Clusterpoint connection
+     */
     function setSource($model,$api_endpoint=null){
         if(!is_array($api_endpoint)){
             $api_endpoint = [$api_endpoint];
@@ -29,21 +35,38 @@ class Controller_Data_Clusterpoint extends Controller_Data {
             $pest->setupAuth($this->app->getConfig('cp/auth/user'), $this->app->getConfig('cp/auth/pass'));
         }
 
-        $model->addMethod('cp', function($m)use($pest){
+        $model->addMethod('cp,search', function($m)use($pest){
             return $pest;
         });
 
         parent::setSource($model, $api_endpoint);
     }
 
+
+    function search($model,$query,$args = []){
+        $d = $this->d($model);
+        $d['search'] = $query;
+    }
+
+
     function _search($model,$query){
         if(!is_array($query)){
             $query = [$query];
         }
 
-        return $this->_advancedCommand($model,'/_search',['query'=>$this->xml_encode($query)]);
+        $d = $this->d($model);
+
+        return $this->_advancedCommand($model,'/_search',['query'=>$d['search'].$this->xml_encode($query)]);
     }
 
+    /**
+     * Some methods in Clusterpoint are specified through XML/POST data
+     *
+     * @param  [type] $model [description]
+     * @param  [type] $url   [description]
+     * @param  [type] $data  [description]
+     * @return [type]        [description]
+     */
     function _advancedCommand($model,$url,$data=null){
         if($model->debug) {
             echo '<font color="blue">'.
@@ -60,53 +83,91 @@ class Controller_Data_Clusterpoint extends Controller_Data {
         }
     }
 
-    function xml_encode($mixed, $domElement=null, $DOMDocument=null) {
-        if (is_null($DOMDocument)) {
-            $DOMDocument =new DOMDocument;
-            $DOMDocument->formatOutput = true;
-            $this->xml_encode($mixed, $DOMDocument, $DOMDocument);
-            $str = $DOMDocument->saveXML();
-            list(,$str) = explode("\n",$str,2);
-            return $str;
-        } else {
-            if (is_array($mixed)) {
-                foreach ($mixed as $index => $mixedElement) {
-                    if (is_int($index)) {
-                        if ($index === 0) {
-                            $node = $domElement;
-                        } else {
-                            $node = $DOMDocument->createElement($domElement->tagName);
-                            $domElement->parentNode->appendChild($node);
-                        }
-                    } else {
-                        $plural = $DOMDocument->createElement($index);
-                        $domElement->appendChild($plural);
-                        $node = $plural;
-                        if (!(rtrim($index, 's') === $index)) {
-                            $singular = $DOMDocument->createElement(rtrim($index, 's'));
-                            $plural->appendChild($singular);
-                            $node = $singular;
-                        }
-                    }
+    function xml_encode($mixed, $xml = null, $debug = false) {
+        if(is_null($xml)){
+           $xml = new SimpleXMLElement("<x></x>");
+           $this->xml_encode($mixed, $xml);
 
-                    return $this->xml_encode($mixedElement, $node, $DOMDocument);
+           if($debug){
+               $dom = dom_import_simplexml($xml)->ownerDocument;
+               $dom->formatOutput = true;
+
+
+               $s=explode("\n",$dom->saveXML(),2);
+
+           }else{
+               $s=explode("\n",$xml->asXML(),2);
+           }
+
+           $s = $s[1];
+           return trim(substr($s,3,-5));
+        }
+
+
+        foreach($mixed as $key => $value) {
+
+            if(is_array($value)) {
+                if(is_numeric($key)){
+                    //$subnode = $xml->addChild("item$key");
+                    $this->xml_encode($value, $xml);
+                } else {
+                    $subnode = $xml->addChild("$key");
+                    $this->xml_encode($value, $subnode);
                 }
             } else {
-                $mixed = is_bool($mixed) ? ($mixed ? 'true' : 'false') : $mixed;
-                $domElement->appendChild($DOMDocument->createTextNode($mixed));
+                if(is_numeric($key)){
+                    $xml[0]=$value;
+                }else{
+                    $xml->addChild("$key",htmlspecialchars("$value"));
+                }
             }
         }
     }
 
     function loadByid($model, $id){
         // TODO: add conditions
-        $data = $this->_search($model, ['id'=>$id]);
+        $data = $this->_search($model, array_merge($this->convertConditions($model->conditions),['id'=>$id]));
         $data = @$data['documents'][0];
         if(is_array($data)) {
             $model->id = $id;
             $model->data = $data;
         }
     }
+
+    function convertConditions($model){
+        $conditions = $model->conditions;
+
+
+        $res = [];
+
+        $res[0] = 'gowrav';
+        foreach($conditions as $rule){
+            list($field, $op, $value) = $rule;
+            switch($op){
+                case'=':
+                case'>':
+                case'<':
+                case'>=':
+                case'<=':
+                case'!=':
+                    $res[$field]=$op.$value;
+            }
+        }
+
+        return $res;
+    }
+
+
+    function loadByConditions($model){
+
+        $data = $this->_search($model, $this->convertConditions($model));
+        $data = @$data['documents'][0];
+        if(is_array($data)) {
+            $model->id = $data[$model->id_field];
+            $model->data = $data;
+        }
+    }
+
 
     function save($model, $id, $data){
         if(is_null($id)){
@@ -130,7 +191,7 @@ class Controller_Data_Clusterpoint extends Controller_Data {
     }
 
     function prefetchAll($model) {
-        $data = $this->_search($model, '*');
+        $data = $this->_search($model, array_merge($this->convertConditions($model)));
         return $data['documents'];
     }
 
