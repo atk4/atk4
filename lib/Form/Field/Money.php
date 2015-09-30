338e16a7939d548bfe08321169570d1363f018d5
Romans Malinovskis <romans@agiletoolkit.org>
2015-09-30 01:29:17 +0100

    add normalization for Money fields to automatically ignore punctuation (UK style)

--- lib/Form/Field/Money.php
+++ lib/Form/Field/Money.php
@@ -5,6 +5,12 @@ class Form_Field_Money extends Form_Field_Number {
         $this->digits = $n;
         return $this;
     }
+	function normalize(){
+		$v = $this->get();
+        // remove non-numbers
+		$v = preg_replace('/[^-0-9\.]/','', $v);
+		$this->set($v);
+	}
     function getInput($attr=array()){
         return parent::getInput(array_merge(array(
                 'value'=>round($this->value,$this->digits)
