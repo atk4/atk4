99c0e715bf8c01578f801e29b49d3b4a2ac3a077
Romans Malinovskis <romans@agiletoolkit.org>
2015-10-30 16:02:11 +0000

    do not rely on old validation ways

--- lib/Form/Field/Number.php
+++ lib/Form/Field/Number.php
@@ -1,24 +1,8 @@
 <?php
 class Form_Field_Number extends Form_Field_Line {
-    public $min = null;
-    public $max = null;
-    
-    function setRange($min,$max){
-        $this->min = $min;
-        $this->max = $max;
-        return $this;
-    }
-    function validate(){
-        // empty value is allowed
-        if($this->value!=''){
-            if(!is_numeric($this->value)) {
-                $this->displayFieldError('Not a valid number');
-            }
-            if( ($this->min!==null && $this->value < $this->min) ||
-                ($this->max!==null && $this->value > $this->max)){
-                $this->displayFieldError('Number not in valid range');
-            }
-        }
-        return parent::validate();
+
+    function setForm($form){
+        parent::setForm($form);
+        $this->validate('number');
     }
 }
