6552c4e4622d9e2bad14119502ab57b4a6a7ce2c
Imants Horsts <imants.horsts@inbox.lv>
2016-02-19 21:07:11 +0200

    App, Auth

--- lib/App/Admin.php
+++ lib/App/Admin.php
@@ -4,35 +4,39 @@
  * model. The benefit is that you'll have access to a number of add-ons which
  * are specifically written for admin system.
  *
- * Exporting add-ons, database migration, test-suites and other add-ons
- * have developed User Interface which can be simply "attached" to your
- * application's admin.
+ * Exporting add-ons, database migration, test-suites and other add-ons have
+ * developed User Interface which can be simply "attached" to your application's
+ * admin.
  *
  * This is done through hooks in the Admin Class. It's also important that
- * App_Admin relies on layout_fluid which makes it easier for add-ons to
- * add menu items, sidebars and foot-bars.
+ * App_Admin relies on layout_fluid which makes it easier for add-ons to add
+ * menu items, sidebars and foot-bars.
  */
-class App_Admin extends App_Frontend {
-
-    public $title='Agile Toolkit™ Admin';
+class App_Admin extends App_Frontend
+{
+    public $title = 'Agile Toolkit™ Admin';
 
     private $controller_install_addon;
 
-    public $layout_class='Layout_Fluid';
+    public $layout_class = 'Layout_Fluid';
 
-    public $auth_config=array('admin'=>'admin');
+    public $auth_config = array('admin' => 'admin');
 
     /** Array with all addon initiators, introduced in 4.3 */
-    private $addons=array();
+    private $addons = array();
 
-    function init() {
+    /**
+     * Initialization.
+     */
+    public function init()
+    {
         parent::init();
         $this->add($this->layout_class);
 
         $this->menu = $this->layout->addMenu('Menu_Vertical');
-        $this->menu->swatch='ink';
+        $this->menu->swatch = 'ink';
 
-        //$m=$this->layout->addFooter('Menu_Horizontal');
+        //$m = $this->layout->addFooter('Menu_Horizontal');
         //$m->addItem('foobar');
 
         $this->add('jUI');
@@ -40,7 +44,11 @@ class App_Admin extends App_Frontend {
         $this->initSandbox();
     }
 
-    private function initSandbox() {
+    /**
+     * @todo Description
+     */
+    private function initSandbox()
+    {
         if ($this->pathfinder->sandbox) {
             $sandbox = $this->app->add('sandbox/Initiator');
 
@@ -50,11 +58,15 @@ class App_Admin extends App_Frontend {
         }
     }
 
-    function initLayout() {
+    /**
+     * @todo Description
+     */
+    public function initLayout()
+    {
         if ($this->pathfinder->sandbox) {
             $this->initAddons();
-        }else{
-            if(preg_match('/^sandbox_/',$this->app->page)){
+        } else {
+            if (preg_match('/^sandbox_/', $this->app->page)) {
                 $this->app->redirect('sandbox');
             }
         }
@@ -65,103 +77,134 @@ class App_Admin extends App_Frontend {
 
         $this->initTopMenu();
 
-
-        if(!$this->pathfinder->sandbox && !$this->app->getConfig('production',false)){
-            $this->menu->addItem(array('Install Developer Toools','icon'=>'tools'),'sandbox');
+        if (!$this->pathfinder->sandbox && !$this->app->getConfig('production', false)) {
+            $this->menu->addItem(array('Install Developer Toools', 'icon' => 'tools'), 'sandbox');
         }
 
-        if(@$this->sandbox){
+        if (@$this->sandbox) {
             $this->sandbox->initLayout();
         }
     }
 
-    function initTopMenu() {
-        $m=$this->layout->add('Menu_Horizontal',null,'Top_Menu');
+    /**
+     * @todo Description
+     */
+    public function initTopMenu()
+    {
+        $m = $this->layout->add('Menu_Horizontal', null, 'Top_Menu');
         //$m->addClass('atk-size-kilo');
-        $m->addItem('Admin','/');
-        $m->addItem('AgileToolkit','/sandbox/dashboard');
-        $m->addItem('Documentation','http://book.agiletoolkit.org/');
-    }
-
-
-
-    function page_sandbox($p){
-        $p->title='Install Developer Tools';
-        //$p->addCrumb('Install Developer Tools');
-
-        $v=$p->add('View',null,null,array('view/developer-tools'));
-
-        $v->add('Button')->set('Install Now')
-            ->addClass('atk-swatch-green')->onClick(function(){
-
-            $installation_dir = getcwd();
-            if(file_exists($installation_dir).'/VERSION'){
-                $installation_dir=dirname($installation_dir);
-            }
-            $path_d = $installation_dir . '/agiletoolkit-sandbox-d.phar';
-            $path = $installation_dir . '/agiletoolkit-sandbox.phar';
-            if (file_put_contents($path_d, file_get_contents("http://www4.agiletoolkit.org/dist/agiletoolkit-sandbox.phar")) == false){
-                return 'update error';
-            }else{
-                if(rename($path_d,$path) == false){
-                    // get version of a phar
-
-                    return 'update error';
-                }else{
-                    $version=file_get_contents('phar://'.$path.'/VERSION');
-
-                    return 'updated to '.$version;
-                }
-            }
-        });
+        $m->addItem('Admin', '/');
+        $m->addItem('AgileToolkit', '/sandbox/dashboard');
+        $m->addItem('Documentation', 'http://book.agiletoolkit.org/');
     }
 
     /**
-     * @return array()
-     * sandbox/Controller_AddonsConfig_Reflection
-     * Return all registered in sandbox_addons.json addons
+     * @todo Description
+     *
+     * @return string
      */
-    function getInstalledAddons() {
+    public function page_sandbox($p)
+    {
+        $p->title = 'Install Developer Tools';
+        //$p->addCrumb('Install Developer Tools');
+
+        $v = $p->add('View', null, null, array('view/developer-tools'));
+
+        $v->add('Button')->set('Install Now')
+            ->addClass('atk-swatch-green')
+            ->onClick(function () {
+                $install_dir = getcwd();
+                if (file_exists($install_dir).'/VERSION') {
+                    $install_dir = dirname($install_dir);
+                }
+                $path_d = $install_dir.'/agiletoolkit-sandbox-d.phar';
+                $path = $install_dir.'/agiletoolkit-sandbox.phar';
+                $url = 'http://www4.agiletoolkit.org/dist/agiletoolkit-sandbox.phar';
+                if (file_put_contents($path_d, file_get_contents($url)) == false) {
+                    return 'update error';
+                } else {
+                    if (rename($path_d, $path) == false) {
+                        // get version of a phar
+
+                        return 'update error';
+                    } else {
+                        $version = file_get_contents('phar://'.$path.'/VERSION');
+
+                        return 'updated to '.$version;
+                    }
+                }
+            });
+    }
+
+    /**
+     * Return all registered in sandbox_addons.json addons
+     * sandbox/Controller_AddonsConfig_Reflection.
+     *
+     * @return array()
+     */
+    public function getInstalledAddons()
+    {
         if (!$this->controller_install_addon) {
             $this->controller_install_addon = $this->add('sandbox\\Controller_InstallAddon');
         }
 
-
-        if ($this->controller_install_addon && $this->controller_install_addon->getSndBoxAddonReader())
+        if ($this->controller_install_addon && $this->controller_install_addon->getSndBoxAddonReader()) {
             return $this->controller_install_addon->getSndBoxAddonReader()->getReflections();
+        }
 
         return array();
     }
 
-    function getInitiatedAddons($addon_api_name=null) {
+    /**
+     * @todo Description
+     *
+     * @param string $addon_api_name
+     *
+     * @return AbstractObject Addon object
+     */
+    public function getInitiatedAddons($addon_api_name = null)
+    {
         if ($addon_api_name) {
             return $this->addons[$addon_api_name];
         }
+
         return $this->addons;
     }
-    private function initAddons() {
+
+    /**
+     * @todo Description
+     */
+    private function initAddons()
+    {
         return;
-        foreach ($this->getInstalledAddons() as $addon) {
-            $this->initAddon($addon);
-        }
+        //foreach ($this->getInstalledAddons() as $addon) {
+        //    $this->initAddon($addon);
+        //}
     }
-    private function initAddon($addon) {
+
+    /**
+     * @todo Description
+     *
+     * @param AbstractObject $addon
+     */
+    private function initAddon($addon)
+    {
         $base_path = $this->pathfinder->base_location->getPath();
         $init_class_path = $base_path.'/../'.$addon->get('addon_full_path').'/lib/Initiator.php';
         if (file_exists($init_class_path)) {
             include $init_class_path;
-            $class_name = str_replace('/','\\',$addon->get('name').'\\Initiator');
-            $init = $this->add($class_name,array(
-                'addon_obj' => $addon,
-                'base_path' => $base_path,
-            ));
-            if (!is_a($init,'Controller_Addon')) {
+            $class_name = str_replace('/', '\\', $addon->get('name').'\\Initiator');
+            $init = $this->add($class_name, array(
+                    'addon_obj' => $addon,
+                    'base_path' => $base_path,
+                ));
+            if (!is_a($init, 'Controller_Addon')) {
                 throw $this->exception(
                     'Initiator of '.$addon->get('name').' is inherited not from \Controller_Addon'
                 );
             }
 
-            /**
+            /*
              * initiators of all addons are accessible
              * from all around the project
              * through $this->app->getInitiatedAddons()
--- lib/App/Installer.php
+++ lib/App/Installer.php
@@ -1,76 +1,78 @@
-<?php // vim:ts=4:sw=4:et:fdm=marker
+<?php
 /**
- * Undocumented
- *
- * @link http://agiletoolkit.org/
-*//*
-==ATK4===================================================
-   This file is part of Agile Toolkit 4
-    http://agiletoolkit.org/
-
-   (c) 2008-2013 Agile Toolkit Limited <info@agiletoolkit.org>
-   Distributed under Affero General Public License v3 and
-   commercial license.
-
-   See LICENSE or LICENSE_COM for more information
- =====================================================ATK4=*/
-class App_Installer extends App_Web {
+ * Undocumented.
+*/
+class App_Installer extends App_Web
+{
     public $page_class = 'Page';
+
     public $page_options = null;
 
     protected $show_intro = false;
 
-    function init(){
+    /**
+     * Initialization.
+     */
+    public function init()
+    {
         parent::init();
 
-        $this->page=null;
+        $this->page = null;
         $this->saved_base_path = $this->pm->base_path;
-        $this->pm->base_path.=basename($_SERVER["SCRIPT_NAME"]);
+        $this->pm->base_path .= basename($_SERVER['SCRIPT_NAME']);
 
         $this->add('jUI');
 
-        $this->template->trySet('version','Web Software Installer');
+        $this->template->trySet('version', 'Web Software Installer');
 
         $this->stickyGET('step');
 
-        $this->s_first=$this->s_last=$this->s_current=$this->s_prev=$this->s_next=null;
-        $this->s_cnt=0;
+        $this->s_first = $this->s_last = $this->s_current = $this->s_prev = $this->s_next = null;
+        $this->s_cnt = 0;
 
         $this->initInstaller();
     }
-    function initInstaller(){
-        //$m=$this->layout->add('Menu');
 
+    /**
+     * @todo Description
+     */
+    public function initInstaller()
+    {
+        //$m = $this->layout->add('Menu');
 
-        foreach(get_class_methods($this) as $method){
-            list($a,$method)=explode('step_',$method); if(!$method)continue;
-            //var_dump($method);
-
-            if(is_null($this->s_first))$this->s_first=$method;
-            $this->s_last=$method;
-
-            $u=$this->url(null,array('step'=>$method));
-            $this->page=$this->pm->base_path.'?step='.$_GET['step'];
-           // $m->addMenuItem($u,ucwords(strtr($method,'_',' ')));
-            $this->page=null;
-
-            if(is_null($this->s_current)){
-                $this->s_cnt++;
-                if($method==$_GET['step']){
-                    $this->s_current=$method;
-                    $this->s_title=ucwords(strtr($method,'_',' '));
-                }else{
-                    $this->s_prev=$method;
-                }
-            }else{
-                if(is_null($this->s_next))$this->s_next=$method;
+        foreach (get_class_methods($this) as $method) {
+            list($a, $method) = explode('step_', $method);
+            if (!$method) {
+                continue;
             }
 
+            if (is_null($this->s_first)) {
+                $this->s_first = $method;
+            }
+            $this->s_last = $method;
 
+            $u = $this->url(null, array('step' => $method));
+            $this->page = $this->pm->base_path.'?step='.$_GET['step'];
+           // $m->addMenuItem($u, ucwords(strtr($method, '_', ' ')));
+            $this->page = null;
+
+            if (is_null($this->s_current)) {
+                ++$this->s_cnt;
+                if ($method == $_GET['step']) {
+                    $this->s_current = $method;
+                    $this->s_title = ucwords(strtr($method, '_', ' '));
+                } else {
+                    $this->s_prev = $method;
+                }
+            } else {
+                if (is_null($this->s_next)) {
+                    $this->s_next = $method;
+                }
+            }
         }
-        if(!$_GET['step']){
 
-            if($this->show_intro) {
+        if (!$_GET['step']) {
+            if ($this->show_intro) {
                 return $this->showIntro($this->makePage('init'));
             }
 
@@ -79,32 +81,66 @@ class App_Installer extends App_Web {
 
         $this->initStep($this->s_current);
     }
-    function makePage($step,$template='step/default') {
-        return $this->layout->add('Page',null,null,array($template));
+
+    /**
+     * @todo Description
+     *
+     * @param [type] $step
+     * @param string $template
+     */
+    public function makePage($step, $template = 'step/default')
+    {
+        return $this->layout->add('Page', null, null, array($template));
     }
-    function initStep($step){
-        $step_method='step_'.$step;
-        if(!$this->hasMethod($step_method))return $this->add('H1')->set('No such step');
-        $this->header=$this->add('H1')->set('Step '.$this->s_cnt.': '.$this->s_title);
+
+    /**
+     * @todo Description
+     *
+     * @param string $step
+     */
+    public function initStep($step)
+    {
+        $step_method = 'step_'.$step;
+        if (!$this->hasMethod($step_method)) {
+            return $this->add('H1')->set('No such step');
+        }
+        $this->header = $this->add('H1')->set('Step '.$this->s_cnt.': '.$this->s_title);
         $page = $this->makePage($step);
+
         return $this->$step_method($page);
     }
-    function showIntro($p){
 
+    /**
+     * @todo Description
+     *
+     * @param View $p
+     */
+    public function showIntro($p)
+    {
         $p->add('H1')->set('Welcome to Web Software');
         $p->add('P')->set('Thank you for downloading this software. This wizard will guide you through the installation procedure.');
 
-        if(!is_writable('.') ){
+        if (!is_writable('.')) {
             $p->add('View_Warning')->setHTML('This installation does not have permissions to create your <b>config.php</b> file for you. You will need to manually create this file');
-        }elseif(file_exists('config.php')){
-            $p->add('View_Warning')->setHTML('It appears that you alerady have <b>config.php</b> file in your applicaiton folder. This installation will read defaults from config.php, but it will ultimatelly <b>overwrite</b> it with the new settings.');
+        } elseif (file_exists('config.php')) {
+            $p->add('View_Warning')->setHTML('It appears that you already have <b>config.php</b> file in your applicaiton folder. This installation will read defaults from config.php, but it will ultimatelly <b>overwrite</b> it with the new settings.');
         }
 
         $p->add('Button')->set('Start')->js('click')->univ()->location($this->stepURL('first'));
     }
-    function stepURL($position){
-        $s='s_'.$position;
-        $s=$this->$s;
-        return $this->url(null,array('step'=>$s));
+
+    /**
+     * @todo Description
+     *
+     * @param string $position
+     *
+     * @return URL
+     */
+    public function stepURL($position)
+    {
+        $s = 's_'.$position;
+        $s = $this->$s;
+
+        return $this->url(null, array('step' => $s));
     }
 }
