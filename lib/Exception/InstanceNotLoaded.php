c81eacc4c77f8b47f7455bec5f3b0c9357884b8c
Imants Horsts <imants.horsts@inbox.lv>
2016-02-25 16:51:53 +0200

    remove unused Exception classes

--- lib/Exception/InstanceNotLoaded.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Thrown wen instance of the class is referenced but not loaded.
- *
- * @author Camper (cmd@adevel.com) on 16.09.2009
- */
-class Exception_InstanceNotLoaded extends Exception_InitError
-{
-}
