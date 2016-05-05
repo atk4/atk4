8693737aba839973731acba3e120b9553bbc3021
Imants Horsts <DarkSide666@users.noreply.github.com>
2016-05-05 16:20:13 +0300

    return empty string as null
    because empty string is not correct numeric format !

--- lib/Form/Field/Number.php
+++ lib/Form/Field/Number.php
@@ -9,16 +9,31 @@ class Form_Field_Number extends Form_Field_Line {
         return $this;
     }
     function validate(){
-        // empty value is allowed
-        if($this->value!=''){
-            if(!is_numeric($this->value)) {
-                $this->displayFieldError('Not a valid number');
-            }
-            if( ($this->min!==null && $this->value < $this->min) ||
-                ($this->max!==null && $this->value > $this->max)){
-                $this->displayFieldError('Number not in valid range');
-            }
+        // empty value is allowed, there should be == instead of ===
+        if($this->value == ''){
+            return parent::validate();
+        }
+        
+        if(!is_numeric($this->value)) {
+            $this->displayFieldError('Not a valid number');
+        }
+        if( ($this->min!==null && $this->value < $this->min) ||
+            ($this->max!==null && $this->value > $this->max)){
+            $this->displayFieldError('Number not in valid range');
         }
         return parent::validate();
+    }
+    /**
+     * Normalize POSTed data
+     *
+     * @return void
+     */
+    function normalize()
+    {
+        // empty value is not correct value for numeric field
+        if ($this->get()==='') {
+            $this->set(null);
+        }
+        return parent::normalize();
     }
 }
