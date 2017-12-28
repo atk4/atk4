eeeb99dfefc6abef1c3d7cc3a7d3fdfd82fd4424
Janis <janis@zoomcharts.com>
2017-12-28 10:55:33 +0200

    PHP compatibility fix; for 4.2

--- lib/ApiWeb.php
+++ lib/ApiWeb.php
@@ -32,7 +32,7 @@ class ApiWeb extends ApiCLI {
 
     // {{{ Start-up 
     function __construct($realm=null,$skin='default'){
-        $this->start_time=time()+microtime();
+        $this->start_time=microtime(true);
 
         $this->skin=$skin;
         try {
@@ -108,11 +108,11 @@ class ApiWeb extends ApiCLI {
     }
     /** @ignore */
     function _showExecutionTime(){
-        echo 'Took '.(time()+microtime()-$this->start_time).'s';
+        echo 'Took '.(microtime(true)-$this->start_time).'s';
     }
     /** @ignore */
     function _showExecutionTimeJS(){
-        echo "\n\n/* Took ".number_format(time()+microtime()-$this->start_time,5).'s */';
+        echo "\n\n/* Took ".number_format(microtime(true)-$this->start_time,5).'s */';
     }
     // }}}
 
